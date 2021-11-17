fea5053ff90def5fea5c477184088bae33d39811
Renoa <38197580+OKAUEND@users.noreply.github.com>
2021-11-17 19:02:00 +0900

    追加 - 選択されたチャンネルと、取得期間の範囲の状態を持つ2つのhookを作成

--- src/features/Archives/api/getArchives.ts
+++ src/features/Archives/api/getArchives.ts
@@ -34,3 +34,20 @@ export const getArchives = <
         `${api_base}?part=${part}&channelId=${channelState}&order=${order}&q=${query}&publishedBefore=${timeRangeState.EndTime}&publishedAfter=${timeRangeState.BeginTime}&maxResults=${maxResult}&key=${APIKey}`
     );
 };
+
+export const currentChannelIDState = atom({
+    key: 'CurrentChannelID',
+    default: 'UC6oDys1BGgBsIC3WhG1BovQ',
+});
+
+const DefaultDate = new Date();
+const HalfMonth = new Date();
+HalfMonth.setMonth(HalfMonth.getMonth() - 6);
+
+export const timeRangeState = atom<timeRangeState>({
+    key: 'TimeRange',
+    default: {
+        EndTime: DefaultDate.toISOString(),
+        BeginTime: HalfMonth.toISOString(),
+    },
+});
