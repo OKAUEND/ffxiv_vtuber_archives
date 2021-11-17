cd980ea479102ec36542c86b6b073bc00ba45039
Renoa <38197580+OKAUEND@users.noreply.github.com>
2021-11-17 19:05:59 +0900

    追加 - 事前に別リポジトリで作成しておいたDOM構造を反映 取得したYoutubeDataをリスト表示する

--- src/features/Archives/component/Archives.tsx
+++ src/features/Archives/component/Archives.tsx
@@ -1,3 +1,41 @@
 import React from 'react';
 import { useRecoilStateLoadable } from 'recoil';
 import { Archives as ArchivesType } from '../types';
+
+type props = {
+    archives: GoogleApiYouTubeSearchResource[];
+};
+
+export const Archives = ({ archives }: props) => {
+    return (
+        <ul className="list-none w-screen">
+            {archives.map((archive) => (
+                <li
+                    key={archive.id.videoId}
+                    className="flex flex-row box-border mr-3">
+                    <div className="w-160 h-90 flex-shrink-0">
+                        <a
+                            href={`https://www.youtube.com/watch?v=${archive.id.videoId}`}
+                            target="_blank"
+                            rel="noreferrer">
+                            <img
+                                src={archive.snippet.thumbnails.medium.url}
+                                className="object-cover w-160 h-90"
+                            />
+                        </a>
+                    </div>
+                    <div className="flex-grow w-1/4">
+                        <div className="flex flex-col ">
+                            <p className="max-h-12 line-clamp-2 text-base">
+                                {archive.snippet.title}
+                            </p>
+                            <div className="text-sm">
+                                {archive.snippet.publishedAt}
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    );
+};
