7a9b63ff84c30608c02737c23117eda079f38e56
Renoa <38197580+OKAUEND@users.noreply.github.com>
2021-11-17 19:00:49 +0900

    追加 - youtube APIへアクセスし非同期処理を返す関数を作成する

--- src/features/Archives/api/getArchives.ts
+++ src/features/Archives/api/getArchives.ts
@@ -15,3 +15,22 @@ type cacheArchives = {
         'items'
     >;
 };
+
+export const getArchives = <
+    T extends GoogleApiYouTubePageInfo<GoogleApiYouTubeSearchResource>
+>(
+    channelState: string,
+    timeRangeState: timeRangeState
+): Promise<AxiosResponse<T, T>> => {
+    const api_base = 'https://www.googleapis.com/youtube/v3/search';
+    const part = 'snippet';
+    const APIKey = '';
+    const maxResult = 50;
+    const order = 'date';
+    const query = 'FF14';
+
+    // &publishedBefore=${endTime}&publishedAfter=${beginTime}
+    return axios.get(
+        `${api_base}?part=${part}&channelId=${channelState}&order=${order}&q=${query}&publishedBefore=${timeRangeState.EndTime}&publishedAfter=${timeRangeState.BeginTime}&maxResults=${maxResult}&key=${APIKey}`
+    );
+};
