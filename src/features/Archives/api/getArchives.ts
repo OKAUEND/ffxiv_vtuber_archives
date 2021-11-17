03bc4724a1d725dda5ad08b32b9916721b6b3da0
Renoa <38197580+OKAUEND@users.noreply.github.com>
2021-11-17 18:58:37 +0900

    追加 - ファイル内でのみ使う型を定義

--- src/features/Archives/api/getArchives.ts
+++ src/features/Archives/api/getArchives.ts
@@ -2,3 +2,16 @@ import axios, { AxiosResponse } from 'axios';
 import { atom, selector } from 'recoil';
 
 import { Archives } from '../types';
+
+type timeRangeState = {
+    EndTime: string;
+    BeginTime: string;
+};
+
+type cacheArchives = {
+    channelId: string;
+    archives?: Pick<
+        GoogleApiYouTubePageInfo<GoogleApiYouTubeSearchResource>,
+        'items'
+    >;
+};
