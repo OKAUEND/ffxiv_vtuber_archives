353958d7c789f639f09a07c673b4b4e4c1fcbec9
Renoa <38197580+OKAUEND@users.noreply.github.com>
2021-11-17 19:08:15 +0900

    追加 - Archiveを表示するための各種ファイルを管理する親コンポーネントを作成

--- src/features/Archives/router/Archives.tsx
+++ src/features/Archives/router/Archives.tsx
@@ -17,3 +17,17 @@ import {
 import { Archives as ArchivesList } from '../component/Archives';
 
 import { Archives as ActiveType } from '../types';
+
+export const Archives = () => {
+    const channelArchives = useRecoilValue(useArchives);
+    const [currentChannelState, setChannelID] = useRecoilState(
+        currentChannelIDState
+    );
+    const [timeRange, setTimeRange] = useRecoilState(timeRangeState);
+
+    return (
+        <div>
+            <ArchivesList archives={channelArchives}></ArchivesList>
+        </div>
+    );
+};
