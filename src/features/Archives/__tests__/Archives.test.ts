1927e8b0e658b60b5183eef3cb97222ff5f825fd
Renoa <38197580+OKAUEND@users.noreply.github.com>
2021-11-28 01:20:46 +0900

    TEST - 毎回テストのデータを作るのが面倒なので、Factory関数で共通化しておく

--- src/features/Archives/__tests__/Archives.test.ts
+++ src/features/Archives/__tests__/Archives.test.ts
@@ -1,5 +1,44 @@
 import { getArchives, setArchives, isPeriod } from '../api/getArchives';
 
+const YoutubeResourcesFactory = (
+    name: string
+): GoogleApiYouTubeSearchResource => {
+    return {
+        kind: name,
+        etag: name,
+        id: {
+            kind: name,
+            videoId: name,
+            channelId: name,
+            playlistId: name,
+        },
+        snippet: {
+            publishedAt: name,
+            channelId: name,
+            title: name,
+            description: name,
+            thumbnails: {
+                default: {
+                    url: name,
+                    width: 99,
+                    height: 99,
+                },
+                high: {
+                    url: name,
+                    width: 99,
+                    height: 99,
+                },
+                medium: {
+                    url: name,
+                    width: 99,
+                    height: 99,
+                },
+            },
+            channelTitle: name,
+        },
+    };
+};
+
 describe('Archives TEST', () => {
     test('対象の時間が基準の時間より大きい場合は真を返す', () => {
         const targetTime = new Date(2021, 12, 3);
