d0e411d5aebfb741577541dcbf8c1850d4e45394
Renoa <38197580+OKAUEND@users.noreply.github.com>
2021-11-27 20:12:20 +0900

    TEST - 単体テストを行うために、ファイルを新規作成

---
+++ src/features/Archives/__tests__/Archives.test.ts
@@ -0,0 +1,10 @@
+import { isPeriod } from '../api/getArchives';
+
+describe('Archives TEST', () => {
+    test('対象の時間が基準の時間より大きい場合は真を返す', () => {
+        const targetTime = new Date(2021, 12, 3);
+        const BeginTime = new Date(2021, 12, 7);
+        const result = isPeriod(targetTime, BeginTime);
+        expect(result).toEqual(true);
+    });
+});
