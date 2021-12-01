89034a9c63ef830c5642dc77d22fc49f745f6ab7
Renoa <38197580+OKAUEND@users.noreply.github.com>
2021-12-01 10:32:41 +0900

    追加 - API通信を行う関数の戻値を通信を行わずにテストしたいため、Mock化するための前準備

--- src/features/Archives/__tests__/Archives.test.ts
+++ src/features/Archives/__tests__/Archives.test.ts
@@ -51,6 +51,19 @@ const YoutubeResourcesFactory = (
 };
 
 describe('Archives TEST', () => {
+    let getSpy: jest.SpyInstance<
+        Promise<GoogleApiYouTubeSearchResource[]>,
+        [channelId: string, TimeRange: { EndTime: string; BeginTime: string }]
+    >;
+
+    beforeEach(async () => {
+        getSpy = jest.spyOn(ArchivesAPIModule, 'fetchYoutube');
+    });
+
+    afterEach(() => {
+        getSpy.mockRestore();
+    });
+
     test('対象の時間が基準の時間より大きい場合は真を返す', () => {
         const targetTime = new Date(2021, 12, 3);
         const BeginTime = new Date(2021, 12, 7);
