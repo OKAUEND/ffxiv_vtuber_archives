5e080da6c108422b40edf75f42d50ec12a2dc3e5
Renoa <38197580+OKAUEND@users.noreply.github.com>
2021-12-13 09:01:13 +0900

    New - 次の内容を読み込むためにユーザーに操作を促す機能

--- src/features/Archives/hook/NextLoad.tsx
+++ src/features/Archives/hook/NextLoad.tsx
@@ -1,5 +1,7 @@
+import {} from 'react';
 import React from 'react';
 
-export const nextLoad = () => {
-    return <div></div>;
+export const NextLoad = () => {
+    const isRender = false;
+    return <div>{isRender === false && <button>次を読み込み</button>}</div>;
 };
