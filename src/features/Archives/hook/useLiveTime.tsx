c4d03fe1d3d92c84047e9f23892feb85e2881eaf
Renoa <38197580+OKAUEND@users.noreply.github.com>
2021-12-16 13:38:22 +0900

    追加 - 取得した動画一覧の末尾にあるデータの放送日時が、初放送日より前かを判定

--- src/features/Archives/hook/useLiveTime.tsx
+++ src/features/Archives/hook/useLiveTime.tsx
@@ -1,9 +1,24 @@
 import { selector, atom, useRecoilState, useRecoilValue } from 'recoil';
+import { useArchives } from '../api/getArchives';
+
 /**
  *初放送日時
  */
 const firstLiveDayTime = atom<Date>({
     key: 'firstLiveDayTime',
     default: new Date(2012, 11, 11),
+});
+
+/**
+ *FFXIV初生放送日時よりも前の日付か
+ */
+const isBeforeFirstLiveDayTime = selector({
+    key: 'isBeforeFirstLiveDayTime',
+    get: ({ get }) => {
+        const firstLiveDayTimeState = get(firstLiveDayTime);
+        const cacheLastArchive = get(useArchives).slice(-1)[0];
+        const cacheLastDayTime = new Date(cacheLastArchive.snippet.publishedAt);
+        return cacheLastDayTime >= firstLiveDayTimeState;
+    },
 });
 
