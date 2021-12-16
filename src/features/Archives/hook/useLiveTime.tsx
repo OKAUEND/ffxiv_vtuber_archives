fea92d4485026b9fe234308c8ae753296f1bfd2e
Renoa <38197580+OKAUEND@users.noreply.github.com>
2021-12-16 13:36:55 +0900

    追加 - チャンネルにおけるFF14を最初に放送した日時を持つ

--- src/features/Archives/hook/useLiveTime.tsx
+++ src/features/Archives/hook/useLiveTime.tsx
@@ -1,1 +1,9 @@
 import { selector, atom, useRecoilState, useRecoilValue } from 'recoil';
+/**
+ *初放送日時
+ */
+const firstLiveDayTime = atom<Date>({
+    key: 'firstLiveDayTime',
+    default: new Date(2012, 11, 11),
+});
+
