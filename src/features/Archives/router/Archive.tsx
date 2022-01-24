651b4bd6b8d944e44e5400f72d6d0543d40e676e
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-01-24 14:48:24 +0900

    追加 - デザインで使用するコンポーネントを読み込み

--- src/features/Archives/router/Archive.tsx
+++ src/features/Archives/router/Archive.tsx
@@ -1,5 +1,8 @@
 import React, { Suspense } from 'react';
 
+import { ArchiveList } from '../component/ArchiveList';
+import { NextLoad } from '../component/NextLoad';
+
 export const Archive = () => {
     return (
         <div>
