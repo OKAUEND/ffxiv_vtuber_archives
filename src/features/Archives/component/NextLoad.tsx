054f861128d6898fd631a93c725cb4a5653dab50
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-01-24 14:31:04 +0900

    追加 - 次の読み込みたい期間をセットしたいので、それを行う関数を作成する

--- src/features/Archives/component/NextLoad.tsx
+++ src/features/Archives/component/NextLoad.tsx
@@ -1,9 +1,17 @@
 import React, { useEffect } from 'react';
 
+import {
+    useRecoilValue,
+    RecoilRoot,
+    useSetRecoilState,
+    useRecoilState,
+} from 'recoil';
 export const NextLoad = () => {
+    const setNextLoadTimeRange = () => {
+    };
     return (
         <div>
-            <button>次をロード</button>
+            <button onClick={setNextLoadTimeRange}>次をロード</button>
         </div>
     );
 };
