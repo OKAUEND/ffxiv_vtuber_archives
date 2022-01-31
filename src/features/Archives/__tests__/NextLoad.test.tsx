cce15d61d7ba31ea01bbe6c9ab1f8a8be8e6f2c4
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-01-31 22:35:07 +0900

    追加 - テストデータを作成したいため、ファクトリー関数を用意し手間を省く

--- src/features/Archives/__tests__/NextLoad.test.tsx
+++ src/features/Archives/__tests__/NextLoad.test.tsx
@@ -4,6 +4,44 @@ import { act, render, screen, waitFor } from '@testing-library/react';
 import NextLoad from '../component/NextLoad';
 
 import * as getYoutubeModule from '../api/getYoutube';
+const YoutubeResourcesFactory = (
+    name: string
+): GoogleApiYouTubeSearchResource => {
+    return {
+        kind: name,
+        etag: name,
+        id: {
+            kind: name,
+            videoId: name,
+            channelId: name,
+            playlistId: name,
+        },
+        snippet: {
+            publishedAt: name,
+            channelId: name,
+            title: name,
+            description: name,
+            thumbnails: {
+                default: {
+                    url: name,
+                    width: 99,
+                    height: 99,
+                },
+                high: {
+                    url: name,
+                    width: 99,
+                    height: 99,
+                },
+                medium: {
+                    url: name,
+                    width: 99,
+                    height: 99,
+                },
+            },
+            channelTitle: name,
+        },
+    };
+};
 describe('Component TEST - NextLoad', () => {
     const spy = jest
         .spyOn(getYoutubeModule, 'useYoutube')
