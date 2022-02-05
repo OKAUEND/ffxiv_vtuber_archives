0bde28bb9fa96147c56bdffb8ef1cba01902eb1b
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-02-06 03:33:27 +0900

    新規 - 読み込ませる期間周りの処理を独立化させ、整理

---
+++ src/features/Archives/hook/useTimeRange.ts
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import {
+    selector,
+    atom,
+    useRecoilState,
+    useRecoilValue,
+    RecoilState,
+} from 'recoil';
+
+export type timeRangetype = {
+    EndTime: string;
+    BeginTime: string;
+};
+
+export const timeRangeAtom = atom<timeRangetype>({
+    key: 'TimeRange',
+    default: {
+        EndTime: '',
+        BeginTime: '',
+    },
+});
+
+export const useTimeRange = () => {
+    const [timeRange, setTimeRange] = useRecoilState(timeRangeAtom);
+
+    useEffect(() => {
+        const realTime = new Date().toISOString();
+        saveTimeRange(realTime);
+    }, []);
+
+    const saveTimeRange = (targetTime: string) => {
+        const lastArchiveTime = new Date(targetTime);
+        lastArchiveTime.setMinutes(lastArchiveTime.getMinutes() - 1);
+        const endTime = lastArchiveTime.toISOString();
+        lastArchiveTime.setMonth(lastArchiveTime.getMonth() - 6);
+        const beginTime = lastArchiveTime.toISOString();
+
+        setTimeRange({
+            EndTime: endTime,
+            BeginTime: beginTime,
+        });
+    };
+
+    return [timeRange, { saveTimeRange }];
+};
