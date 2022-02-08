1c3c7d88d3d0b0b9e7b26318f520099fa9d073ae
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-02-08 23:21:12 +0900

    変更 - nextloadを大改修したので、引数と渡す関数を設定しておく

--- src/features/Archives/router/Archive.tsx
+++ src/features/Archives/router/Archive.tsx
@@ -12,6 +12,15 @@ type Props = {
 export const Archive = (props: Props) => {
     const [, saveTimeRange] = useTimeRange();
     const [Archives] = useArchives(props.channelId);
+    const saveLiveTimeRange = () => {
+        console.log('');
+    };
+
+    const storeArchives = (
+        youtubeArchives: GoogleApiYouTubeSearchResource[]
+    ) => {
+        console.log('');
+    };
     return (
         <div>
             <div>
@@ -19,7 +28,11 @@ export const Archive = (props: Props) => {
             </div>
             <div>
                 <Suspense fallback={<p>Loading...</p>}>
-                    <NextLoad />
+                    <NextLoad
+                        channelId=""
+                        onClick={saveLiveTimeRange}
+                        store={storeArchives}
+                    />
                 </Suspense>
             </div>
         </div>
