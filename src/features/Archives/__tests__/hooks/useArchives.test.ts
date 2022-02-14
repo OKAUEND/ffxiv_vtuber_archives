b64345c28f223fd0645f90ea0b290dd6b967a03d
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-02-14 22:06:03 +0900

    追加 - renderhookを使い、カスタムフックのテストの第一歩を行う

--- src/features/Archives/__tests__/hooks/useArchives.test.ts
+++ src/features/Archives/__tests__/hooks/useArchives.test.ts
@@ -1,2 +1,10 @@
-import {} from '@testing-library/react-hooks';
-import {} from '../../hook/useArchives';
+import { RenderResult, renderHook, act } from '@testing-library/react-hooks';
+import { useArchives } from '../../hook/useArchives';
+
+describe('useArchives TEST', () => {
+    test('初期値の何も格納がされていない配列であること', () => {
+        const { result } = renderHook(() => useArchives('testChannel'));
+        const [target] = result.current;
+        expect(target).toEqual([]);
+    });
+});
