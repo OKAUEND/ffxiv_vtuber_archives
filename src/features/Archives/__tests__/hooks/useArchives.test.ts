59c0b3b74441432e14de7f9a0de7be4654d01f39
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-02-15 21:17:13 +0900

    追加 - テストデータを作成するファクトリー関数を作成

--- src/features/Archives/__tests__/hooks/useArchives.test.ts
+++ src/features/Archives/__tests__/hooks/useArchives.test.ts
@@ -1,6 +1,45 @@
 import { RenderResult, renderHook, act } from '@testing-library/react-hooks';
 import { useArchives } from '../../hook/useArchives';
 
+const YoutubeResourcesFactory = (
+    name: string
+): GoogleApiYouTubeSearchResource => {
+    return {
+        kind: name,
+        etag: name,
+        id: {
+            kind: name,
+            videoId: name,
+            channelId: name,
+            playlistId: name,
+        },
+        snippet: {
+            publishedAt: name,
+            channelId: name,
+            title: name,
+            description: name,
+            thumbnails: {
+                default: {
+                    url: name,
+                    width: 99,
+                    height: 99,
+                },
+                high: {
+                    url: name,
+                    width: 99,
+                    height: 99,
+                },
+                medium: {
+                    url: name,
+                    width: 99,
+                    height: 99,
+                },
+            },
+            channelTitle: name,
+        },
+    };
+};
+
 describe('useArchives TEST', () => {
     test('初期値の何も格納がされていない配列であること', () => {
         const { result } = renderHook(() => useArchives('testChannel'));
