77844ca5e0bca834c978f52f68b44096c8472fea
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-02-16 22:38:26 +0900

    追加 - 新しいテスト項目を追加する

--- src/features/Archives/__tests__/hooks/useTimeRange.test.ts
+++ src/features/Archives/__tests__/hooks/useTimeRange.test.ts
@@ -1,2 +1,14 @@
 import { RenderResult, renderHook, act } from '@testing-library/react-hooks';
 import { useTimeRange } from '../../hook/useTimeRange';
+
+describe('useArchives TEST', () => {
+    test('初期値として時刻が現在日時セットされている', () => {
+        const { result } = renderHook(() => useTimeRange());
+        const [timeRange] = result.current;
+
+        expect(timeRange).toBe(true);
+    });
+
+    test('新しい時間を渡すと、重複取得防止として、1分前の時間がセットされている', () => {});
+    test('新しい時間を渡すと、6ヶ月前の日時がセットされている', () => {});
+});
