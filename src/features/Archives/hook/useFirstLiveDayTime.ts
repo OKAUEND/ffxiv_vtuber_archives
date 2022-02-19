a6005ee23df7d2027bf02ec567a0cc1f2340dd81
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-02-19 22:40:35 +0900

    追加 - 初回放送日前かどうかを判定するカスタムフックを作成

--- src/features/Archives/hook/useFirstLiveDayTime.ts
+++ src/features/Archives/hook/useFirstLiveDayTime.ts
@@ -1,1 +1,22 @@
-export const useFirstLiveDayTime = () => {};
+import { useEffect } from 'react';
+import { atom, useRecoilState } from 'recoil';
+
+const firstLiveDayTimeAtom = atom<string>({
+    key: 'firstLiveDayTimeAtom',
+    default: '',
+});
+
+export const useFirstLiveDayTime = (firstLiveDayTime: string) => {
+    const [criteriaDayTime, setFirstLiveDayTime] =
+        useRecoilState(firstLiveDayTimeAtom);
+
+    useEffect(() => {
+        setFirstLiveDayTime(firstLiveDayTime);
+    }, [firstLiveDayTime]);
+
+    const isBeforeFirstDayTime = (targetDayTime: string) => {
+        return new Date(criteriaDayTime) > new Date(targetDayTime);
+    };
+
+    return [isBeforeFirstDayTime];
+};
