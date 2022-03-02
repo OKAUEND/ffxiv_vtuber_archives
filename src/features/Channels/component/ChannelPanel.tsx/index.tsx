042d077ab01176fdc54d2a8268fbece89e9aa888
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-03-02 21:56:01 +0900

    変更 - ベースとして表示したい要素を書き出す

--- src/features/Channels/component/ChannelPanel.tsx/index.tsx
+++ src/features/Channels/component/ChannelPanel.tsx/index.tsx
@@ -1,5 +1,12 @@
 import React, { Suspense, useEffect, useState } from 'react';
 
 export const Channels = () => {
-    return <div></div>;
+    return (
+        <div>
+            <img src="" />
+            {
+                //チャンネル名
+            }
+        </div>
+    );
 };
