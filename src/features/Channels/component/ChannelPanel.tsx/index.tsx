899bd2ccbd8cefb9439040e4476e990d46a4c478
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-03-02 22:04:59 +0900

    変更 - いくつものチャンネルを表示するのが前提となるため、リスト要素を指定しておく

--- src/features/Channels/component/ChannelPanel.tsx/index.tsx
+++ src/features/Channels/component/ChannelPanel.tsx/index.tsx
@@ -2,11 +2,15 @@ import React, { Suspense, useEffect, useState } from 'react';
 
 export const ChannelPanel = () => {
     return (
-        <div>
-            <img src="" />
-            {
-                //チャンネル名
-            }
-        </div>
+        <ul>
+            <li>
+                <div>
+                    <img src="" />
+                    {
+                        //チャンネル名
+                    }
+                </div>
+            </li>
+        </ul>
     );
 };
