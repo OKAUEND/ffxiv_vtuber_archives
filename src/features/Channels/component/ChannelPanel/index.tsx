26a6c23659edc543897209a0f8a4b5490d0de41e
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-03-03 22:13:36 +0900

    変更 - フォルダ名に拡張子がはいっていたので、変更

---
+++ src/features/Channels/component/ChannelPanel/index.tsx
@@ -0,0 +1,26 @@
+import React, { Suspense, useEffect, useState } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// interface IProps {
+//     Channels: string[];
+// }
+
+export const ChannelPanel = () => {
+    return (
+        // <ul>
+        //     {Channels.map((channel) => (
+        //         //まだ受け渡すオブジェクトの構造ができあがっていないので、
+        //         //仮コード
+        //         <li key={channel}>
+        //             <div>
+        //                 <img src="" />
+        //                 {
+        //                     //チャンネル名
+        //                 }
+        //             </div>
+        //         </li>
+        //     ))}
+        // </ul>
+        <h2>TEST</h2>
+    );
+};
