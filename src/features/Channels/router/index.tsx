6a3e7f8b554c7ea56afcff769cd0fcd477c2e151
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-03-03 22:13:58 +0900

    試し - ReactRouterを試しに使ってみる

--- src/features/Channels/router/index.tsx
+++ src/features/Channels/router/index.tsx
@@ -1,5 +1,15 @@
 import React, { Suspense, useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { ChannelPanel } from '../component/ChannelPanel';
 
 export const Channels = () => {
-    return <div></div>;
+    return (
+        <div>
+            <div>
+                <Routes>
+                    <Route path="/" element={<ChannelPanel />} />
+                </Routes>
+            </div>
+        </div>
+    );
 };
