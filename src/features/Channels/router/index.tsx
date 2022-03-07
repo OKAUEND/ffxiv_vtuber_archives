bcc589e5ab1a8ee9cd3294de24e6680fb7b23ca7
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-03-07 20:58:48 +0900

    試し - ダイナミックルーティングでLink要素のtoに変数を当てた場合の挙動確認

--- src/features/Channels/router/index.tsx
+++ src/features/Channels/router/index.tsx
@@ -1,18 +1,24 @@
 import React, { Suspense, useEffect, useState } from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes, Link, useParams } from 'react-router-dom';
 import { ChannelPanel } from '../component/ChannelPanel';
 
 export const Channels = () => {
+    const testId = 'UCFpxoltilHCmuHWeERqsUlA';
     return (
         <div>
             <div>
-                <Link to="hogehogepa-">TEST LINK</Link>
+                <Link to={testId}>
+                    <ChannelPanel></ChannelPanel>
+                </Link>
                 <Routes>
-                    <Route
-                        path="/:channelId"
-                        element={<ChannelPanel />}></Route>
+                    <Route path="/:channelId" element={<Page1 />}></Route>
                 </Routes>
             </div>
         </div>
     );
+};
+
+const Page1 = () => {
+    const { channelId } = useParams();
+    return <h2>TEST {channelId}</h2>;
 };
