a57d81c2e174a88c28359196d35ac807277d592c
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-03-08 18:56:14 +0900

    変更 - 取得した値をキャッシュするためのAtomを追加

--- src/features/Channels/api/getChannels.ts
+++ src/features/Channels/api/getChannels.ts
@@ -1,2 +1,9 @@
 import { atom } from 'recoil';
+import { HikasenVtuber } from '../types/index';
+
+const ChannelsAtom = atom<HikasenVtuber[]>({
+    key: 'ChannelsAtom',
+    default: [],
+});
+
 export const useChannels = () => {};
