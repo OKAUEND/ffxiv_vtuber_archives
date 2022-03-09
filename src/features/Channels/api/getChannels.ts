fde03dcf66228dabda4c8ca48487228149ae6ab9
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-03-09 21:49:52 +0900

    変更 - axiosの設定をしたいので、インスタンスを作成する関数を作っておく 他のと共有できるから、これだけBaseで切り出してもいいかも

--- src/features/Channels/api/getChannels.ts
+++ src/features/Channels/api/getChannels.ts
@@ -3,6 +3,14 @@ import { useEffect } from 'react';
 import { atom, useRecoilState } from 'recoil';
 import { HikasenVtuber } from '../types/index';
 
+const axiosGASInstance = axios.create({
+    baseURL: '',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    timeout: 2000,
+});
+
 const ChannelsAtom = atom<HikasenVtuber[]>({
     key: 'ChannelsAtom',
     default: [],
