fe60f99a948b1249bdb92a3902c7fe19f7103e1b
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-03-09 21:50:45 +0900

    作業中 - 3月9日の作業途中のをコミットしておく axiosのレスポンスにあるanyをどう排除するかで試行錯誤中 型をがっちり決めてやるべき？

--- src/features/Channels/api/getChannels.ts
+++ src/features/Channels/api/getChannels.ts
@@ -16,4 +16,28 @@ const ChannelsAtom = atom<HikasenVtuber[]>({
     default: [],
 });
 
-export const useChannels = () => {};
+interface IResponse {
+    id: string;
+    name: string;
+}
+
+const fetchChannels = async () => {
+    try {
+        const url = '';
+        const response = await axiosGASInstance.get<IResponse>(url);
+
+        if (response.data.id) return [];
+        return response.data;
+    } catch (err) {
+        if (err instanceof Error) {
+            console.error(err.message);
+        }
+    }
+};
+
+export const useChannels = () => {
+    const [channels, setChannels] = useRecoilState(ChannelsAtom);
+    useEffect(() => {
+        setChannels(fetchChannels());
+    }, []);
+};
