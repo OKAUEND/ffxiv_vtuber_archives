281d267a334dac7629e55b2679a44f4f5831517f
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-03-13 20:39:02 +0900

    お試し - Githubのコードを参考にaxiosのmockを試すが、型情報で詰まる 次の日は別のにやってみるか、型情報をきちんとさせる

--- src/features/Channels/__tests__/api/channelAPI.test.ts
+++ src/features/Channels/__tests__/api/channelAPI.test.ts
@@ -1,2 +1,31 @@
-import axios from 'axios';
-import { axiosGASInstance } from '../../api/getChannels';
+import axios, { AxiosAdapter } from 'axios';
+import { fetchChannels } from '../../api/getChannels';
+const mock = undefined as unknown as AxiosAdapter;
+
+jest.mock('mock', () => jest.fn());
+
+const axiosMockAdapter = mock as unknown as jest.Mock<
+    ReturnType<AxiosAdapter>,
+    Parameters<AxiosAdapter>
+>;
+
+describe('Channel Get API TEST', () => {
+    beforeEach(() => {
+        axiosMockAdapter.mockClear();
+    });
+    test('Axios interceptors resolve時の反応をみる', () => {
+        const response = {
+            status: 401,
+        };
+
+        const request = {
+            name: 'hogehoge',
+        };
+
+        axiosMockAdapter.mockResolvedValueOnce(response);
+
+        const rejectedResponse =
+            axios.interceptors.response.handlers[0].rejected(response);
+        expect(axiosGASInstance.interceptors.response.handlers[0]);
+    });
+});
