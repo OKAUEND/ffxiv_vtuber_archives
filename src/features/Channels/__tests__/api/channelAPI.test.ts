d76308ce3b2a3ea5948e2ebb447f9853f7025840
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-03-15 23:17:18 +0900

    作業中 - axiosのmockお試し3日目 responseがundefindになるため、モックがうまくいっていないので、もっと調べる

--- src/features/Channels/__tests__/api/channelAPI.test.ts
+++ src/features/Channels/__tests__/api/channelAPI.test.ts
@@ -1,10 +1,24 @@
 import axios, { AxiosAdapter } from 'axios';
 import { fetchChannels } from '../../api/getChannels';
 
-jest.mock('axios');
+const mockAPI = jest.fn().mockName('mock-api');
+jest.mock('axios', () => ({
+    __esModule: true,
+    default: {
+        create: jest.fn(() => {
+            return {
+                interceptors: {
+                    request: { use: jest.fn() },
+                    response: { use: jest.fn() },
+                },
+                post: mockAPI,
+            };
+        }),
+    },
+}));
 const mockAxios = axios as jest.Mocked<typeof axios>;
 
-describe('Channel Get API TEST', async () => {
+describe('Channel Get API TEST', () => {
     test('Axios interceptors resolve時の反応をみる', async () => {
         const successResponse = {
             status: 200,
