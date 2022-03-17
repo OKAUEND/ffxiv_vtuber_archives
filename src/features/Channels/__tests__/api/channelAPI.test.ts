6ca661d3d2b0d7ba8ad07578ec2f584aa3336968
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-03-17 21:17:54 +0900

    変更 - 使わなくなったmockの宣言を削除

--- src/features/Channels/__tests__/api/channelAPI.test.ts
+++ src/features/Channels/__tests__/api/channelAPI.test.ts
@@ -3,9 +3,6 @@ import { axiosGASInstance } from '../../api/getChannels';
 import mockAdapter from 'axios-mock-adapter';
 import { fetchChannels } from '../../api/getChannels';
 
-jest.mock('axios');
-const mockAxios = axios as jest.Mocked<typeof axios>;
-
 describe('Channel Get API TEST', () => {
     test('Axios interceptors resolve時の反応をみる', async () => {
         const mock = new mockAdapter(axiosGASInstance);
