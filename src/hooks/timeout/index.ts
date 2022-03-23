00c3c83e75488f478d8a3a13bce0bf5e8654d991
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-03-23 21:42:21 +0900

    変更 - 引数の型を一旦宣言

--- src/hooks/timeout/index.ts
+++ src/hooks/timeout/index.ts
@@ -1,5 +1,12 @@
 import { useEffect } from 'react';
 import { atom, useRecoilState } from 'recoil';
+
+export type AxiosResut = {
+    status: number;
+    errorCode: string;
+    error: boolean;
+    payload: object;
+};
 export const useTimeOutError = () => {
     return [];
 };
