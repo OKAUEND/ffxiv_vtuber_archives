5e8ba205e9fae99944a51633df7a73f0533b6670
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-03-27 20:54:31 +0900

    変更 - hooksで変更した、タイムアウトエラー時の対応をカスタムフック側で受け取る処理へ変更

--- src/hooks/timeout/index.ts
+++ src/hooks/timeout/index.ts
@@ -1,16 +1,16 @@
 import { useEffect } from 'react';
 import { atom, useRecoilState } from 'recoil';
-import { AxiosResut } from '../../types/api';
+import { AxiosResut } from '../../types/api/index';
 
 const timeOutErrorAtom = atom<boolean>({
     key: 'timeouterror',
     default: false,
 });
 
-export const useTimeOutError = (target: AxiosResut) => {
+export const useTimeOutError = (target: AxiosResut<[]>) => {
     const [isTimeOut, setState] = useRecoilState(timeOutErrorAtom);
     useEffect(() => {
-        if (target.errorCode == 'TIMEOUT_ERROR') {
+        if (target.errorCode == 'ECONNABORTED' && target.error) {
             setState(target.error);
         }
     }, [target.status, target.error]);
