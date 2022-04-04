19d0d8871783083886fac3422eda3191efb279d5
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-04-04 13:55:31 +0900

    変更 - テストに必要なライブラリやモジュールをインポートする

--- src/features/Channels/__tests__/component/channelPanel.test.ts
+++ src/features/Channels/__tests__/component/channelPanel.test.ts
@@ -7,6 +7,14 @@ import {
     waitFor,
 } from '@testing-library/react';
 import { ChannelPanel } from '../../component/ChannelPanel';
+import { useChannels } from '../../api/getChannels';
+import { useTimeOutError } from '../../../../hooks/timeout/index';
+import { Channels } from 'features/Channels/router';
+
+import * as getChannelsModule from '../../api/getChannels';
+import { HikasenVtuber } from '../..//types/index';
+import { AxiosResut } from '../../../../types/api/index';
+
 
 describe('channel Panel - コンポーネントテスト', () => {
     test('', () => {});
