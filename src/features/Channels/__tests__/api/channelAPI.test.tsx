005f229846733a94d604f9c78a91d2dd7edae235
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-04-11 21:20:26 +0900

    変更 - RecoilRootを使用するかもなので、拡張子をtsxに変更をする

--- src/features/Channels/__tests__/api/channelAPI.test.tsx
+++ src/features/Channels/__tests__/api/channelAPI.test.tsx
@@ -1,49 +1,68 @@
 import axios from 'axios';
+import React from 'react';
 import { RenderResult, renderHook, act } from '@testing-library/react-hooks';
+import { RecoilRoot } from 'recoil';
 import { axiosGASInstance } from '../../api/getChannels';
 import mockAdapter from 'axios-mock-adapter';
 import { useChannels, fetchChannels } from '../../api/getChannels';
+import * as fetchChannelModule from '../../api/getChannels';
 
 import { HikasenVtuber } from '../../types/index';
 import { AxiosResut } from '../../../../types/api/index';
+import { waitFor } from '@testing-library/react';
+
+const HikasenVtuberResourceFactory = (name: string): HikasenVtuber => {
+    return {
+        channelID: name,
+        channelIconID: name,
+        name: name,
+        twitter: '',
+        twitch: '',
+        ffxiv: {
+            dataCenter: 'test',
+            server: 'test',
+        },
+    };
+};
+
+const AxiosStatusFactory = (
+    status: number,
+    isSuccess: boolean,
+    HikasenVtuber: HikasenVtuber[]
+): AxiosResut<HikasenVtuber[]> => {
+    const errorCode = () => {
+        if (status === 408) {
+            return 'ECONNABORTED';
+        }
+        return 'TEST';
+    };
+
+    return {
+        status: status,
+        errorCode: errorCode(),
+        error: isSuccess,
+        payload: HikasenVtuber,
+    };
+};
 
 describe('Channel Get API TEST', () => {
     test('Axios interceptors resolve時の反応をみる', async () => {
         const mock = new mockAdapter(axiosGASInstance);
-
         const successResponse = {
             id: 1,
             name: 'hogefuga',
         };
-
         const request = {
             name: 'hogehoge',
         };
-
         mock.onPost('/channel').replyOnce(200, successResponse);
         const result = await fetchChannels();
-
         expect(result).toStrictEqual(successResponse);
     });
-
     test('Axios interceptors reject時の反応をみる', async () => {
         const mock = new mockAdapter(axiosGASInstance);
-
-        mock.onPost('/channel').replyOnce(400, 'error');
+        mock.onPost('/channel').replyOnce(408, 'error');
         const result = await fetchChannels();
-
         expect(result).toStrictEqual('error');
-    });
-
-    test('リロードの関数を使用したら、取得関数がコールされるか', () => {
-        const mock = new mockAdapter(axiosGASInstance);
-        const { result } = renderHook(() => useChannels());
-        const [channels, resultStatus, loadData] = result.current;
-
-        mock.onPost('/channel').replyOnce(200, 'success');
-        act(async () => {
-            loadData();
-            await expect(fetchChannels()).toHaveBeenCalled();
-        });
     });
 });
