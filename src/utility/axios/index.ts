1e0792dcbe5b4aa1e68574494ae6b8be55ceb3aa
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-04-14 21:18:15 +0900

    変更 - すでに組み上げたコードを移植する 型はジェネリクスを使用し、推論できるようにする

--- src/utility/axios/index.ts
+++ src/utility/axios/index.ts
@@ -1,1 +1,35 @@
-import axios, { AxiosError } from 'axios';
+import axios, { AxiosAdapter, AxiosError, AxiosResponse } from 'axios';
+import { AxiosResut } from '../../types/api/index';
+
+const axiosInstance = <T, P>() => {
+    const axiosGASInstance = axios.create({
+        baseURL: '',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        timeout: 2000,
+    });
+
+    const onSuccessful = (response: AxiosResponse<T[]>): AxiosResut<T[]> => {
+        const result: AxiosResut<T[]> = {
+            status: response.status,
+            payload: response.data,
+        };
+        return result;
+    };
+
+    const onRejected = (error: AxiosError) => {
+        if (error.response?.data === 'ECONNABORTED') {
+            const result: AxiosResut<T[]> = {
+                status: 408,
+                error: true,
+                errorCode: error.code,
+                payload: [],
+            };
+            return result;
+        }
+        return Promise.reject('error');
+    };
+
+    return axiosGASInstance.interceptors.response.use(onSuccessful, onRejected);
+};
