332576ba25f1b90bb4dd94ace345b167b48f6c1f
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-04-14 21:32:13 +0900

    変更 - Axiosインスタンスを生成していた処理を、共通モジュールに変更する

--- src/features/Channels/api/getChannels.ts
+++ src/features/Channels/api/getChannels.ts
@@ -2,39 +2,8 @@ import axios, { AxiosError } from 'axios';
 import { useEffect } from 'react';
 import { atom, useRecoilState } from 'recoil';
 import { HikasenVtuber } from '../types/index';
+import { AxiosInstance } from '../../../utility/axios';
 import { AxiosResut } from '../../../types/api/index';
-
-export const axiosGASInstance = axios.create({
-    baseURL: '',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    timeout: 2000,
-});
-
-const onSuccessful = (response: AxiosResponse): AxiosResut<HikasenVtuber[]> => {
-    const result: AxiosResut<HikasenVtuber[]> = {
-        status: response.status,
-        payload: response.data,
-    };
-    return result;
-};
-
-const onRejected = (error: AxiosError) => {
-    if (error.response?.data === 'ECONNABORTED') {
-        const result: AxiosResut<HikasenVtuber[]> = {
-            status: 408,
-            error: true,
-            errorCode: error.code,
-            payload: [],
-        };
-        return result;
-    }
-
-    return Promise.reject('error');
-};
-
-axiosGASInstance.interceptors.response.use(onSuccessful, onRejected);
 
 const ChannelsAtom = atom<HikasenVtuber[]>({
     key: 'ChannelsAtom',
@@ -46,24 +15,17 @@ const ResultStatus = atom<Omit<AxiosResut<HikasenVtuber[]>, 'payload'>>({
     default: { status: 200 },
 });
 
-export const fetchChannels = async () => {
-    const url = '';
-    const response = await axiosGASInstance.post<
-        string,
-        AxiosResut<HikasenVtuber[]>
-    >('/channel');
-    return response;
-};
-
 export const useChannels = () => {
+    const fetchPost = AxiosInstance<HikasenVtuber>();
     const [channels, setChannels] = useRecoilState(ChannelsAtom);
     const [resultStatus, setresultStatus] = useRecoilState(ResultStatus);
+
     useEffect(() => {
         loadData();
     }, []);
 
     const loadData = async () => {
-        const result = await fetchChannels();
+        const result = await fetchPost('/channel');
         setChannels(result.payload);
         setresultStatus(result);
     };
