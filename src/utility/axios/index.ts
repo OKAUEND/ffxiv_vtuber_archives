4fadc1b41d1a634f67b44af892f776732a3f624e
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-04-16 22:04:55 +0900

    変更 - テストがしづらかったので、それぞれ個別に変更

--- src/utility/axios/index.ts
+++ src/utility/axios/index.ts
@@ -1,42 +1,38 @@
 import axios, { AxiosAdapter, AxiosError, AxiosResponse } from 'axios';
 import { AxiosResut } from '../../types/api/index';
 
-export const AxiosInstance = <T>() => {
-    const axiosInstance = axios.create({
-        baseURL: '',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        timeout: 2000,
-    });
+const axiosInstance = axios.create({
+    baseURL: '',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    timeout: 2000,
+});
 
-    const onSuccessful = (response: AxiosResponse<T[]>): AxiosResut<T[]> => {
+const onSuccessful = <T>(response: AxiosResponse<T[]>): AxiosResut<T[]> => {
+    const result: AxiosResut<T[]> = {
+        status: response.status,
+        payload: response.data,
+    };
+    return result;
+};
+
+const onRejected = <T>(error: AxiosError) => {
+    if (error.response?.data === 'ECONNABORTED') {
         const result: AxiosResut<T[]> = {
-            status: response.status,
-            payload: response.data,
+            status: 408,
+            error: true,
+            errorCode: error.code,
+            payload: [],
         };
         return result;
-    };
+    }
+    return Promise.reject('error');
+};
 
-    const onRejected = (error: AxiosError) => {
-        if (error.response?.data === 'ECONNABORTED') {
-            const result: AxiosResut<T[]> = {
-                status: 408,
-                error: true,
-                errorCode: error.code,
-                payload: [],
-            };
-            return result;
-        }
-        return Promise.reject('error');
-    };
+axiosInstance.interceptors.response.use(onSuccessful, onRejected);
 
-    axiosInstance.interceptors.response.use(onSuccessful, onRejected);
-
-    const fetchPost = async (url: string) => {
-        const response = await axiosInstance.post<string, AxiosResut<T[]>>(url);
-        return response;
-    };
-
-    return fetchPost;
+export const fetchPost = async <T>(url: string) => {
+    const response = await axiosInstance.post<string, AxiosResut<T[]>>(url);
+    return response;
 };
