1213e1250975d9a1d653f4020f6cdcaa111cf2f7
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-04-18 20:53:50 +0900

    変更 - Axiosにかかわる処理を共通化しファイルを切り出したため、移したテスト内容を削除する

--- src/features/Channels/__tests__/api/channelAPI.test.tsx
+++ src/features/Channels/__tests__/api/channelAPI.test.tsx
@@ -45,28 +45,7 @@ const AxiosStatusFactory = (
 };
 
 describe('Channel Get API TEST', () => {
-    test('Axios interceptors resolve時の反応をみる', async () => {
-        const mock = new mockAdapter(axiosGASInstance);
-        const successResponse = {
-            id: 1,
-            name: 'hogefuga',
-        };
-        const request = {
-            name: 'hogehoge',
-        };
-        mock.onPost('/channel').replyOnce(200, successResponse);
-        const result = await fetchChannels();
-        expect(result).toStrictEqual(successResponse);
-    });
-    test('Axios interceptors reject時の反応をみる', async () => {
-        const mock = new mockAdapter(axiosGASInstance);
-        mock.onPost('/channel').replyOnce(408, 'ECONNABORTED');
-        const result = await fetchChannels();
-        expect(result.status).toStrictEqual(408);
-    });
-
     test('リロードの関数を使用したら、取得関数がコールされるか', async () => {
-
         const mock = jest
             .spyOn(getChannelsModule, 'fetchChannels')
             .mockImplementationOnce(() =>
