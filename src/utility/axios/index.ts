bc4038e212eba27a38c19268fe6527c6783a76a9
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-04-21 20:07:54 +0900

    変更 - インスタンスを関数化し、引数でaxiosの設定を変更できるように

--- src/utility/axios/index.ts
+++ src/utility/axios/index.ts
@@ -1,38 +1,49 @@
 import axios, { AxiosAdapter, AxiosError, AxiosResponse } from 'axios';
 import { AxiosResut } from '../../types/api/index';
 
-export const axiosInstance = axios.create({
-    baseURL: '',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    timeout: 2000,
-});
+export const AxiosInstance = (contentType: contentType) => {
+    const instance = axios.create({
+        baseURL: '',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': contentType,
+        },
+        timeout: 2000,
+    });
 
-const onSuccessful = <T>(response: AxiosResponse<T[]>): AxiosResut<T[]> => {
-    const result: AxiosResut<T[]> = {
-        status: response.status,
-        payload: response.data,
-    };
-    return result;
-};
-
-const onRejected = <T>(error: AxiosError) => {
-    if (error.response?.data === 'ECONNABORTED') {
+    const onSuccessful = <T>(response: AxiosResponse<T[]>): AxiosResut<T[]> => {
         const result: AxiosResut<T[]> = {
-            status: 408,
-            error: true,
-            errorCode: error.code,
-            payload: [],
+            status: response.status,
+            payload: response.data,
         };
+
         return result;
-    }
-    return Promise.reject('error');
-};
+    };
 
-axiosInstance.interceptors.response.use(onSuccessful, onRejected);
+    const onRejected = <T>(error: AxiosError) => {
+        if (error.response?.data === 'ECONNABORTED') {
+            const result: AxiosResut<T[]> = {
+                status: 408,
+                error: true,
+                errorCode: error.code,
+                payload: [],
+            };
+            return result;
+        }
+        return Promise.reject('error');
+    };
 
-export const fetchPost = async <T>(url: string) => {
-    const response = await axiosInstance.post<string, AxiosResut<T[]>>(url);
-    return response;
+    instance.interceptors.response.use(onSuccessful, onRejected);
+
+    const fetchPost = async <T>(url: string) => {
+        const response = await instance.post<string, AxiosResut<T[]>>(url);
+        return response;
+    };
+
+    const get = async <T>(url: string) => {
+        const response = await instance.get<string, AxiosResut<T[]>>(url);
+        return response;
+    };
+
+    return [fetchPost, get];
 };
