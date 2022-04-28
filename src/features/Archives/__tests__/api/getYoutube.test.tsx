f75ef42e57761ef3f29c2c8990cd8501610af537
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-04-28 17:35:49 +0900

    変更 - テスト内容を作成

--- src/features/Archives/__tests__/api/getYoutube.test.tsx
+++ src/features/Archives/__tests__/api/getYoutube.test.tsx
@@ -5,3 +5,8 @@ import { RecoilRoot } from 'recoil';
 import * as AxiosInstanceModule from '../../../../utility/axios/index';
 import { AxiosResut } from '../../../../types/api/index';
 import { waitFor } from '@testing-library/react';
+
+describe('getYoutube Custom Hook TEST', () => {
+    test('初回作成時に、APIをコールし値を取得できているか', () => {});
+    test('クエリ作成関数をコールしたら、APIをコールし値を取得できるか', () => {});
+});
