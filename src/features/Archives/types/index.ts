b281d13c58d61135f5903a1f0d829e92ae32160e
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-04-30 21:01:07 +0900

    変更 - ファイルを横断して型を使うため、型のファイルへ情報を移す

--- src/features/Archives/types/index.ts
+++ src/features/Archives/types/index.ts
@@ -2,3 +2,8 @@ export type Archives = Pick<
     GoogleApiYouTubePageInfo<GoogleApiYouTubeSearchResource>,
     'items'
 >;
+
+export type timeRangetype = {
+    EndTime: string;
+    BeginTime: string;
+};
