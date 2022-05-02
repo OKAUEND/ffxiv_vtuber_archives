2794637719581d9b5419c703a80616a433f3c836
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-05-02 21:22:50 +0900

    変更 - renderhookを置き換えつつ、mockを渡し、インスタンスを同一にしておく

--- src/features/Archives/__tests__/hooks/useTimeRange.test.ts
+++ src/features/Archives/__tests__/hooks/useTimeRange.test.ts
@@ -11,19 +11,15 @@ const useMock = () => {
 
 describe('useArchives TEST', () => {
     test('初期値として時刻が現在日時セットされている', () => {
-        const { result } = renderHook(() => useTimeRange(), {
-            wrapper: RecoilRoot,
-        });
-        const [timeRange] = result.current;
+        const { result } = renderRecoilHook(useMock);
+        const { state } = result.current;
 
-        expect(timeRange).toBe(true);
+        expect(state).not.toBe({});
     });
 
     test('新しい時間を渡すと、重複取得防止として、1分前の時間がセットされている', () => {
-        const { result } = renderHook(() => useTimeRange(), {
-            wrapper: RecoilRoot,
-        });
-        const [timeRange, createTimeRange] = result.current;
+        const { result } = renderRecoilHook(useMock);
+        const { state, createTimeRange } = result.current;
 
         const testDayTime = '20200101';
 
@@ -34,13 +30,11 @@ describe('useArchives TEST', () => {
         const testDate = new Date(testDayTime);
         testDate.setMinutes(testDate.getMinutes() - 1);
 
-        expect(timeRange.EndTime).toBe(testDate.toISOString());
+        expect(state.EndTime).toBe(testDate.toISOString());
     });
     test('新しい時間を渡すと、6ヶ月前の日時がセットされている', () => {
-        const { result } = renderHook(() => useTimeRange(), {
-            wrapper: RecoilRoot,
-        });
-        const [timeRange, createTimeRange] = result.current;
+        const { result } = renderRecoilHook(useMock);
+        const { state, createTimeRange } = result.current;
 
         const testDayTime = '20200101';
 
@@ -51,6 +45,6 @@ describe('useArchives TEST', () => {
         const testDate = new Date(testDayTime);
         testDate.setMinutes(testDate.getMonth() - 6);
 
-        expect(timeRange.EndTime).toBe(testDate.toISOString());
+        expect(state.EndTime).toBe(testDate.toISOString());
     });
 });
