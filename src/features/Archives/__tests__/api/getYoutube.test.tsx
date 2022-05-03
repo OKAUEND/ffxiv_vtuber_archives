7a27f0ba08a6e3fa5211438e9c813fa0cc3d4a16
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-05-03 20:59:30 +0900

    変更 - mockの返し値を作るファクリー関数を作成

--- src/features/Archives/__tests__/api/getYoutube.test.tsx
+++ src/features/Archives/__tests__/api/getYoutube.test.tsx
@@ -6,6 +6,10 @@ import * as AxiosInstanceModule from '../../../../utility/axios/index';
 import { AxiosResut } from '../../../../types/api/index';
 import { waitFor } from '@testing-library/react';
 
+const GoogleYoutubeType = (
+    name: string
+): GoogleApiYouTubePageInfo<GoogleApiYouTubeSearchResource> => {};
+
 describe('getYoutube Custom Hook TEST', () => {
     test('初回作成時に、APIをコールし値を取得できているか', () => {});
     test('クエリ作成関数をコールしたら、APIをコールし値を取得できるか', () => {});
