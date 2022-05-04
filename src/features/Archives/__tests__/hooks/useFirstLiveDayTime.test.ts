b5819035bb723dcc372a5b3ba63383520b1b5654
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-05-05 00:19:57 +0900

    変更 - テスト内容を作成

--- src/features/Archives/__tests__/hooks/useFirstLiveDayTime.test.ts
+++ src/features/Archives/__tests__/hooks/useFirstLiveDayTime.test.ts
@@ -14,11 +14,37 @@ const useMock = () => {
 };
 
 describe('useFirstLiveDayTime TEST', () => {
-    test('初回生成時に、初回放送日の値を保持していること');
-    test(
-        'isBeforeFirstDayTimeに初放送日以後の現在時刻を渡すと、初放送日後なので偽値が返ってくる事'
-    );
-    test(
-        'isBeforeFirstDayTimeに初放送日以前の現在時刻を渡すと、初放送日前なので真値が返ってくる事'
-    );
+    test('初回生成時に、初回放送日の値を保持していること', async () => {
+        const { result } = renderRecoilHook(useMock);
+
+        await waitFor(() => {
+            const { state } = result.current;
+
+            expect(state).toEqual(testDayTime);
+        });
+    });
+    test('isBeforeFirstDayTimeに初放送日以後の現在時刻を渡すと、初放送日後なので偽値が返ってくる事', async () => {
+        const { result } = renderRecoilHook(useMock);
+
+        await waitFor(() => {
+            const { isBeforeFirstDayTime, state } = result.current;
+            expect(state).toEqual(testDayTime);
+            act(() => {
+                const isEnable = isBeforeFirstDayTime('2021-01');
+                expect(isEnable).toBe(false);
+            });
+        });
+    });
+    test('isBeforeFirstDayTimeに初放送日以前の現在時刻を渡すと、初放送日前なので真値が返ってくる事', async () => {
+        const { result } = renderRecoilHook(useMock);
+
+        await waitFor(() => {
+            const { isBeforeFirstDayTime, state } = result.current;
+            expect(state).toEqual(testDayTime);
+            act(() => {
+                const isEnable = isBeforeFirstDayTime('2019-01');
+                expect(isEnable).toBe(true);
+            });
+        });
+    });
 });
