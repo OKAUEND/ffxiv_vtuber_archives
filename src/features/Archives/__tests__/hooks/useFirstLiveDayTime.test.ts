e64ea8c23146c11dabfdbfb4ccba69ab1980b536
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-05-04 21:24:23 +0900

    変更 - テスト項目を作成

--- src/features/Archives/__tests__/hooks/useFirstLiveDayTime.test.ts
+++ src/features/Archives/__tests__/hooks/useFirstLiveDayTime.test.ts
@@ -4,3 +4,13 @@ import {
     useFirstLiveDayTime,
     firstLiveDayTimeAtom,
 } from '../../hook/useFirstLiveDayTime';
+
+describe('useFirstLiveDayTime TEST', () => {
+    test('初回生成時に、初回放送日の値を保持していること');
+    test(
+        'isBeforeFirstDayTimeに初放送日以後の現在時刻を渡すと、初放送日後なので偽値が返ってくる事'
+    );
+    test(
+        'isBeforeFirstDayTimeに初放送日以前の現在時刻を渡すと、初放送日前なので真値が返ってくる事'
+    );
+});
