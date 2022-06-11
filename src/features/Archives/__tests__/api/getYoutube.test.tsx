2651fe184bbd4334cfb4817ab07d13983ee071cd
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-06-11 20:52:44 +0900

    変更 - 分かれていた機能を1つにまとめたので、テストファイルも1つにまとめる

--- src/features/Archives/__tests__/api/getYoutube.test.tsx
+++ src/features/Archives/__tests__/api/getYoutube.test.tsx
@@ -95,3 +95,43 @@ describe('getYoutube Custom Hook TEST', () => {
         });
     });
 });
+
+const useArchivesMock = () => {
+    const [, , addArchives, exists] = useArchives('testchannel');
+    const ArchiveAtom = useRecoilValue(archivesAtom('testchannel'));
+    return { addArchives, exists, ArchiveAtom };
+};
+
+describe('useArchives TEST', () => {
+    test('初期値の何も格納がされていない配列であること', () => {
+        const { result } = renderRecoilHook(useMock);
+        expect(result.current.ArchiveAtom).toEqual([]);
+    });
+    test('addArchivesへ新しい配列を渡すと、その値を保存するか', () => {
+        const { result } = renderRecoilHook(useMock);
+        expect(result.current.ArchiveAtom).toEqual([]);
+
+        const testData = YoutubeResourcesFactory('test');
+
+        act(() => {
+            result.current.addArchives([testData]);
+        });
+
+        expect(result.current.ArchiveAtom).toEqual([testData]);
+    });
+
+    test('existsでは、値の有無で真偽値が返ってくるか', () => {
+        const { result } = renderRecoilHook(useMock);
+        expect(result.current.ArchiveAtom).toEqual([]);
+        expect(result.current.exists()).toEqual(false);
+
+        const testData = YoutubeResourcesFactory('test');
+
+        act(() => {
+            result.current.addArchives([testData]);
+        });
+
+        expect(result.current.ArchiveAtom).toEqual([testData]);
+        expect(result.current.exists()).toEqual(true);
+    });
+});
