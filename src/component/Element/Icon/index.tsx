fc0bdbcf59e964c81d7b3fcc5025eee664e0d101
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-06-22 13:10:21 +0900

    変更 - アイコンサイズの型情報を作成する

--- src/component/Element/Icon/index.tsx
+++ src/component/Element/Icon/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+//アイコンのサイズをPropsで指定したいので型の内容を制限する
+type size = 'Large' | 'Medium' | 'Small';
+
 export const IconElement = (): JSX.Element => {
     return <div></div>;
 };
