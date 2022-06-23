8107b6e259227b2618b28cb0dcc19fc5894d99c4
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-06-23 12:50:20 +0900

    変更 - imgの要素を取得し、それに自ら指定したい型を追加で設定する方法に変更

--- src/component/Element/Icon/index.tsx
+++ src/component/Element/Icon/index.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 //アイコンのサイズをPropsで指定したいので型の内容を制限する
 type size = 'Large' | 'Medium' | 'Small';
 
-interface Props {
-    desc: string;
-    path: string;
+type ImageProps = JSX.IntrinsicElements['img'];
+
+type Props = ImageProps & {
     size: size;
     isRadius: boolean;
-}
+};
 
 export const IconElement = (props: Props): JSX.Element => {
     return (
