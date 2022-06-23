c97723a8d03d3265b8372dbc25ce8398c2d12d31
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-06-23 12:44:46 +0900

    変更 - Propsを作成する

--- src/component/Element/Icon/index.tsx
+++ src/component/Element/Icon/index.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 //アイコンのサイズをPropsで指定したいので型の内容を制限する
 type size = 'Large' | 'Medium' | 'Small';
 
-export const IconElement = (): JSX.Element => {
+interface Props {
+    size: size;
+    isRadius: boolean;
+}
+
+export const IconElement = (props: Props): JSX.Element => {
     return <div></div>;
 };
