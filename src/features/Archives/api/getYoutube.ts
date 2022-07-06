8c1f03730f7fb0a5b074d8a9fabef60d787e6905
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-07-06 18:52:00 +0900

    変更 - URLを作成する関数を使用するように

--- src/features/Archives/api/getYoutube.ts
+++ src/features/Archives/api/getYoutube.ts
@@ -84,7 +84,7 @@ export const youtubeSelector = selectorFamily<
 
             if (requestQuery === '') return [];
 
-            const requestURL = `https://www.googleapis.com/youtube/v3/search?channelId=${channelId}${requestQuery}`;
+            const requestURL = createYoutubeURL(channelId, requestQuery);
 
             const request = await axiosGet<
                 GoogleApiYouTubePaginationInfo<GoogleApiYouTubeSearchResource>
