61da520d6266bc96b6b6a1e77772b3b5b5f25898
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-07-12 20:15:51 +0900

    変更 - デザインの構成を簡単に作りたいので、gridへ変更

--- src/features/Archives/component/ArchiveList.tsx
+++ src/features/Archives/component/ArchiveList.tsx
@@ -11,24 +11,26 @@ export const ArchiveList = ({ Archives }: IProps) => {
                 <li
                     key={index}
                     className="flex flex-row box-border mr-3 mt-3 p-6">
-                    <div className="w-160 h-90 flex-shrink-0">
-                        <a
-                            href={`https://www.youtube.com/watch?v=${archive.id.videoId}`}
-                            target="_blank"
-                            rel="noreferrer">
-                            <img
-                                src={archive.snippet.thumbnails.medium.url}
-                                className="object-cover w-160 h-90"
-                            />
-                        </a>
-                    </div>
-                    <div className="flex-grow w-1/4 flex justify-left items-center pl-4">
-                        <div className="flex flex-col ">
-                            <p className="max-h-12 line-clamp-2 text-base">
-                                {archive.snippet.title}
-                            </p>
-                            <div className="text-sm">
-                                {archive.snippet.publishedAt}
+                    <div className="grid grid-row-3 grid-flow-col gap-2">
+                        <div className="row-span-3">
+                            <a
+                                href={`https://www.youtube.com/watch?v=${archive.id.videoId}`}
+                                target="_blank"
+                                rel="noreferrer">
+                                <img
+                                    src={archive.snippet.thumbnails.medium.url}
+                                    className="object-cover w-160 h-90"
+                                />
+                            </a>
+                        </div>
+                        <div className="row-span-3 col-span-1 flex justify-center items-center">
+                            <div className="flex flex-col ">
+                                <h3 className="max-h-12 line-clamp-2 text-base text-gray-100">
+                                    {archive.snippet.title}
+                                </h3>
+                                <div className="text-sm text-gray-400 ">
+                                    {archive.snippet.publishedAt}
+                                </div>
                             </div>
                         </div>
                     </div>
