2d267b2f45205e689a254bdbd4fc1f610f5ffe49
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-07-14 19:45:15 +0900

    変更 - gridで中央配置にするため、div要素を追加

--- src/features/Archives/component/ArchiveList.tsx
+++ src/features/Archives/component/ArchiveList.tsx
@@ -6,36 +6,41 @@ interface IProps {
 
 export const ArchiveList = ({ Archives }: IProps) => {
     return (
-        <ul className="list-none w-screen">
-            {Archives.map((archive, index) => (
-                <li
-                    key={index}
-                    className="flex flex-row box-border mr-3 mt-3 p-6">
-                    <div className="grid grid-row-3 grid-flow-col gap-2">
-                        <div className="row-span-3">
-                            <a
-                                href={`https://www.youtube.com/watch?v=${archive.id.videoId}`}
-                                target="_blank"
-                                rel="noreferrer">
-                                <img
-                                    src={archive.snippet.thumbnails.medium.url}
-                                    className="object-cover w-160 h-90"
-                                />
-                            </a>
-                        </div>
-                        <div className="row-span-3 col-span-1 flex justify-center items-center">
-                            <div className="flex flex-col ">
-                                <h3 className="max-h-12 line-clamp-2 text-base text-gray-100">
-                                    {archive.snippet.title}
-                                </h3>
-                                <div className="text-sm text-gray-400 ">
-                                    {archive.snippet.publishedAt}
+        <div className="grid grid-cols-3">
+            <ul className="list-none w-screen">
+                {Archives.map((archive, index) => (
+                    <li
+                        key={index}
+                        className="flex flex-row box-border mr-3 mt-3 p-6">
+                        <div className="grid grid-row-3 grid-flow-col gap-2">
+                            <div className="row-span-3">
+                                <a
+                                    href={`https://www.youtube.com/watch?v=${archive.id.videoId}`}
+                                    target="_blank"
+                                    rel="noreferrer">
+                                    <img
+                                        src={
+                                            archive.snippet.thumbnails.medium
+                                                .url
+                                        }
+                                        className="object-cover w-160 h-90"
+                                    />
+                                </a>
+                            </div>
+                            <div className="row-span-3 col-span-1 flex justify-center items-center">
+                                <div className="flex flex-col ">
+                                    <h3 className="max-h-12 line-clamp-2 text-base text-gray-100 truncate">
+                                        {archive.snippet.title}
+                                    </h3>
+                                    <div className="text-sm text-gray-400 ">
+                                        {archive.snippet.publishedAt}
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </li>
-            ))}
-        </ul>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 };
