9b7b51d77faab1903626bc0da63395a8f5c0f45e
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-07-19 20:13:03 +0900

    変更 - ボタンサイズを動的に変更できるように

--- src/component/Element/Button/hover.tsx
+++ src/component/Element/Button/hover.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
+type size = 'small' | 'medium' | 'large';
+
+const ElementSize = (size: size): string => {
+    switch (size) {
+        case 'small':
+            return '';
+        case 'medium':
+            return '';
+        case 'large':
+            return '';
+    }
+};
 export const hover = () => {
     return <button></button>;
 };
