4cd0ba02bb2859bc89a1a8b3ea9934f579e3de2f
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-07-20 20:03:56 +0900

    変更 - 戻るの文字から、左矢印へデザインを変更

--- src/features/Archives/router/index.tsx
+++ src/features/Archives/router/index.tsx
@@ -3,8 +3,10 @@ import React, { Suspense } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { Archive } from './Archive';
+
 import NextLoad from '../component/NextLoad';
 import { Hover } from '../../../component/Element/Button';
+import { LeftArrow } from '../../../component/Element/Icon';
 
 const typeGuard = (target: string | undefined) => {
     if (target === undefined) {
@@ -23,8 +25,10 @@ export const ArchiveRouter = () => {
     return (
         <div>
             <div>
-                <Hover handler={() => navigate(-1)} size="medium" radius="full">
-                    戻る
+                <Hover handler={() => navigate(-1)} size="small" radius="full">
+                    <div className="text-gray-100">
+                        <LeftArrow />
+                    </div>
                 </Hover>
             </div>
             <div>
