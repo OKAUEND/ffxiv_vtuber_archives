077d2c4d9efe7f5985c6470ddc1f0b87f5d01e1c
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-07-22 19:31:04 +0900

    変更 - ボタンのサイズを動的に変更するための関数と型を追加

--- src/component/Element/Button/basic.tsx
+++ src/component/Element/Button/basic.tsx
@@ -1,5 +1,17 @@
 import React, { ReactNode } from 'react';
 
+type size = 'small' | 'medium' | 'large';
+
+const ElementSize = (size: size): string => {
+    switch (size) {
+        case 'small':
+            return 'w-16 h-8';
+        case 'medium':
+            return 'w-28 h-14';
+        case 'large':
+            return 'w-40 h-20';
+    }
+};
 
 export const Basic = () => {
     return <button></button>;
