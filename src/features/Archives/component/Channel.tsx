88cb8ac97e9eba7db15bc5d98f209a0c0fe49419
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-07-31 08:00:33 +0900

    変更 - 必要なモジュールや型情報を読み込む

--- src/features/Archives/component/Channel.tsx
+++ src/features/Archives/component/Channel.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { IconElement } from 'component/Element/Image';
+import { VtuberDetail } from '../types';
 
 export const Channel = () => {
     return <div></div>;
