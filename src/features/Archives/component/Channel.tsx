ba1b18bfba42f770f5870501050dbb37544545a8
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-07-31 08:01:10 +0900

    変更 - 環境変数でYoutubeのURLを読み込む

--- src/features/Archives/component/Channel.tsx
+++ src/features/Archives/component/Channel.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { IconElement } from 'component/Element/Image';
 import { VtuberDetail } from '../types';
 
+const iconURL = import.meta.env.VITE_YOUTUBE_CHANNEL_ICON_URL;
+
 export const Channel = () => {
     return <div></div>;
 };
