46bc4321f372ef2ddcca2f22ce5936252b36ceac
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-07-31 08:01:58 +0900

    変更 - Propsの状態を元に、ベースの構成を作成する

--- src/features/Archives/component/Channel.tsx
+++ src/features/Archives/component/Channel.tsx
@@ -4,6 +4,17 @@ import { VtuberDetail } from '../types';
 
 const iconURL = import.meta.env.VITE_YOUTUBE_CHANNEL_ICON_URL;
 
-export const Channel = () => {
-    return <div></div>;
+export const Channel = (channelDetail: VtuberDetail) => {
+    return (
+        <div>
+            <IconElement
+                src={`${iconURL}${channelDetail.channelIconID}`}
+                size="Medium"
+                radius="full"></IconElement>
+            <span>{channelDetail.channelName}</span>
+            {channelDetail.twitter}
+            {channelDetail.youtube}
+            {channelDetail.twitch}
+        </div>
+    );
 };
