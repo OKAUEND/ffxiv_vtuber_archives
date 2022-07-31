4296f2b6bc0675ce98431f72f5775783a1f45008
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-07-31 11:41:01 +0900

    新規 - Twitterのアイコンを新規作成

--- src/component/Element/Icon/index.tsx
+++ src/component/Element/Icon/index.tsx
@@ -1,5 +1,6 @@
 import { LeftArrow } from './LeftArrow';
 import { Youtube } from './Youtube';
 import { Twitch } from './Twitch';
+import { Twitter } from './Twitter';
 
-export { LeftArrow, Youtube, Twitch };
+export { LeftArrow, Youtube, Twitch, Twitter };
