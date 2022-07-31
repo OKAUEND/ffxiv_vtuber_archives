c76fffeefde04aca50283b0939e62cd425d884dc
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-07-31 11:45:17 +0900

    変更 - アイコン要素を読み込み、外部リンクを作成する

--- src/features/Archives/component/Channel.tsx
+++ src/features/Archives/component/Channel.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IconElement } from 'component/Element/Image';
 import { VtuberDetail } from '../types';
 
+import { Twitter, Youtube, Twitch } from '../../../component/Element/Icon';
 const iconURL = import.meta.env.VITE_YOUTUBE_CHANNEL_ICON_URL;
 
 export const Channel = (channelDetail: VtuberDetail) => {
@@ -12,9 +13,15 @@ export const Channel = (channelDetail: VtuberDetail) => {
                 size="Medium"
                 radius="full"></IconElement>
             <span>{channelDetail.channelName}</span>
-            {channelDetail.twitter}
-            {channelDetail.channelID}
-            {channelDetail.twitch}
+            <a href={channelDetail.twitter}>
+                <Twitter />
+            </a>
+            <a href={channelDetail.channelID}>
+                <Youtube />
+            </a>
+            <a href={channelDetail.twitch}>
+                <Twitch />
+            </a>
         </div>
     );
 };
