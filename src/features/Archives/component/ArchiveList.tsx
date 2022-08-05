06bd1006e6181981a2a1b7e173864536fc0a6cf5
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-08-05 12:51:03 +0900

    変更 - 一番外で囲っていたdivが不要だったため、削除

--- src/features/Archives/component/ArchiveList.tsx
+++ src/features/Archives/component/ArchiveList.tsx
@@ -11,43 +11,36 @@ export const ArchiveList = ({ Archives }: IProps) => {
     };
 
     return (
-        <div>
-            <ul className="list-none w-screen col-start-2 col-end-3 max-w-full">
-                {Archives.map((archive, index) => (
-                    <li
-                        key={index}
-                        className="box-border md:bg-gray-700 mt-3 p-6 md:rounded-lg max-w-full">
-                        <div className="flex flex-col md:flex-row">
-                            <div className="min-w-160 flex-shrink-0">
-                                <a
-                                    href={`https://www.youtube.com/watch?v=${archive.id.videoId}`}
-                                    target="_blank"
-                                    rel="noreferrer">
-                                    <img
-                                        src={
-                                            archive.snippet.thumbnails.medium
-                                                .url
-                                        }
-                                        className="object-cover w-160 h-90"
-                                    />
-                                </a>
-                            </div>
-                            <div className="flex justify-center items-center flex-shrink ml-2">
-                                <div className="flex flex-col ">
-                                    <span className="h-12 text-base text-gray-100 text-left line-clamp-2 mt-2 md:mt-0">
-                                        {archive.snippet.title}
-                                    </span>
-                                    <div className="text-sm text-gray-400 text-left mt-2">
-                                        {converDayTime(
-                                            archive.snippet.publishedAt
-                                        )}
-                                    </div>
+        <ul className="list-none w-screen col-start-2 col-end-3 max-w-full">
+            {Archives.map((archive, index) => (
+                <li
+                    key={index}
+                    className="box-border md:bg-gray-700 mt-3 p-6 md:rounded-lg max-w-full">
+                    <div className="flex flex-col md:flex-row">
+                        <div className="min-w-160 flex-shrink-0">
+                            <a
+                                href={`https://www.youtube.com/watch?v=${archive.id.videoId}`}
+                                target="_blank"
+                                rel="noreferrer">
+                                <img
+                                    src={archive.snippet.thumbnails.medium.url}
+                                    className="object-cover w-160 h-90"
+                                />
+                            </a>
+                        </div>
+                        <div className="flex justify-center items-center flex-shrink ml-2">
+                            <div className="flex flex-col ">
+                                <span className="h-12 text-base text-gray-100 text-left line-clamp-2 mt-2 md:mt-0">
+                                    {archive.snippet.title}
+                                </span>
+                                <div className="text-sm text-gray-400 text-left mt-2">
+                                    {converDayTime(archive.snippet.publishedAt)}
                                 </div>
                             </div>
                         </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
+                    </div>
+                </li>
+            ))}
+        </ul>
     );
 };
