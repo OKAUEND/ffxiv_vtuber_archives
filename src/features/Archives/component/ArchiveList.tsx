832d7ffb0eca3bff39d5ad2ccf475e2ca9dd3eee
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-08-13 13:40:06 +0900

    変更 - 上の要素と近く、視認性が悪いので余白を設ける

--- src/features/Archives/component/ArchiveList.tsx
+++ src/features/Archives/component/ArchiveList.tsx
@@ -19,7 +19,7 @@ export const ArchiveList = ({ Archives }: IProps) => {
             {Archives.map((archive, index) => (
                 <li
                     key={index}
-                    className="box-border md:bg-gray-700 md:mt-3 pl-3 pr-3 md:p-6 md:rounded-lg max-w-full">
+                    className="box-border md:bg-gray-700 mt-3 pl-3 pr-3 md:p-6 md:rounded-lg max-w-full">
                     <div className="flex flex-col md:flex-row">
                         <div className="min-w-160 md:w-2/4 m-0 flex-shrink-0 text-center">
                             <a
