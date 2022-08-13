ac347d914f860ead93622895b957d1f479789a4f
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-08-13 13:38:44 +0900

    変更 - indexファイル内でSuspenseやRecoilRoot等の設定を行う

--- src/features/Archives/router/index.tsx
+++ src/features/Archives/router/index.tsx
@@ -1,3 +1,13 @@
+import React, { Suspense } from 'react';
+import { RecoilRoot } from 'recoil';
 import { Archive } from './Archive';
 
-export { Archive };
+export const Archives = () => {
+    return (
+        <RecoilRoot>
+            <Suspense fallback={<p>Loading...</p>}>
+                <Archive />
+            </Suspense>
+        </RecoilRoot>
+    );
+};
