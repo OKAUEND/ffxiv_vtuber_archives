2069eb3ca0b8334e0248e5e39d1b1a8191999a99
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-08-18 16:44:22 +0900

    変更 - mockの戻値として使うデータを作成する関数

--- src/utility/mock/index.ts
+++ src/utility/mock/index.ts
@@ -1,6 +1,51 @@
 import { AxiosInstance } from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
+const GoogleYoutubeFactory = (
+    channelId: string
+): GoogleApiYouTubePageInfo<GoogleApiYouTubeSearchResource> => {
+    return {
+        kind: channelId,
+        etag: channelId,
+        items: [
+            {
+                kind: channelId,
+                etag: channelId,
+                id: {
+                    kind: channelId,
+                    videoId: channelId,
+                    channelId: channelId,
+                    playlistId: channelId,
+                },
+                snippet: {
+                    publishedAt: channelId,
+                    channelId: channelId,
+                    title: channelId,
+                    description: channelId,
+                    thumbnails: {
+                        default: {
+                            url: channelId,
+                            width: 99,
+                            height: 99,
+                        },
+                        high: {
+                            url: channelId,
+                            width: 99,
+                            height: 99,
+                        },
+                        medium: {
+                            url: channelId,
+                            width: 99,
+                            height: 99,
+                        },
+                    },
+                    channelTitle: channelId,
+                },
+            },
+        ],
+    };
+};
+
 const run = (client: AxiosInstance, url: string) => {
     const mock = new MockAdapter(client);
     mock.onGet(url).reply(200, [GoogleYoutubeFactory('')]);
