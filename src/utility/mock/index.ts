a97d59dfe4c288104744a184644c60b9cf4c596c
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-08-18 16:43:54 +0900

    変更 - urlの自動生成は難しいので、呼び出し元から使用するurlをそのまま渡すようにする

--- src/utility/mock/index.ts
+++ src/utility/mock/index.ts
@@ -1,7 +1,9 @@
 import { AxiosInstance } from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
-const run = (client: AxiosInstance) => {
+const run = (client: AxiosInstance, url: string) => {
     const mock = new MockAdapter(client);
-    mock.onGet().reply(200);
+    mock.onGet(url).reply(200, [GoogleYoutubeFactory('')]);
 };
+
+export { run };
