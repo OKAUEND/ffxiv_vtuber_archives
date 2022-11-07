271b44cc53559c2df405e6a2def2d8a513aac7ff
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-11-07 14:01:22 +0900

    変更 - routerからコードを移設し、統合させる

---
+++ src/features/Archives/pages/index.tsx
@@ -0,0 +1,44 @@
+import React, { Suspense } from 'react';
+
+import link from 'next/link';
+
+import NextLoad from '../component/NextLoad';
+
+import { ArchiveList } from '../component/ArchiveList';
+import { useArchives } from '../api/getYoutube';
+import { useFirstLiveDayTime } from '../hook/useFirstLiveDayTime';
+
+const typeGuard = (target: string | undefined) => {
+    if (target === undefined) {
+        return 'error';
+    }
+    return target;
+};
+
+export const ArchiveRouter = () => {
+    // const { channelID } = useParams<'channelID'>();
+    // const navigate = useNavigate();
+
+    //TypeGuardでundefinedを除外する
+    // const targetChannelID = typeGuard(channelID);
+
+    const [Archives] = useArchives('channelId');
+
+    return (
+        <div>
+            <div>
+                {/* <button onClick={() => navigate(-1)}>戻る</button> */}
+            </div>
+            <div>
+                <Suspense fallback={<p>Loading...</p>}>
+                    <ArchiveList Archives={Archives} />
+                </Suspense>
+            </div>
+            <div>
+                <Suspense fallback={<p>Loading...</p>}>
+                    <NextLoad channelId={'targetChannelID'} />
+                </Suspense>
+            </div>
+        </div>
+    );
+};
