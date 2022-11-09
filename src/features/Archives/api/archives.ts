e7b6a69d3b50e78e2f0cc65fa8f7773f9b868dd3
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-11-09 15:26:41 +0900

    変更 - YoutubeDateAPIをコールする部分の処理を書く

--- src/features/Archives/api/archives.ts
+++ src/features/Archives/api/archives.ts
@@ -2,6 +2,25 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 type Handler = (request: NextApiRequest, response: NextApiResponse) => void;
 
-const handler: Handler = (request, response) => {};
+const handler: Handler = async (request, response) => {
+    const { method } = request;
+    switch (method) {
+        case 'GET':
+            try {
+                const res = await axios.get(
+                    `https://www.googleapis.com/youtube/v3/search?channelId=${'channelId'}${'query'}`
+                );
+            } catch (e) {
+                console.error('Request error', e);
+                response.status(500).json({ error: 'Error fetchng posts' });
+            }
+            break;
+
+        default:
+            response.setHeader('Allow', ['GET']);
+            response.status(405).end(`Method ${method} Not Allowed`);
+            break;
+    }
+};
 
 export default handler;
