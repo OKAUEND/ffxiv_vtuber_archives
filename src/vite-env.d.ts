6a1cd5fdc5655285db34cca19f01389162f648bb
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-11-12 18:24:43 +0900

    変更 - 名称をより抽象的にするべく、SPREADSHEETを取り除く

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,6 @@
 
 interface ImportMetaEnv {
     readonly VITE_YOUTUBE_API: string;
-    readonly VITE_CHANNELLIST_SPREADSHEET_URL: string;
+    readonly VITE_CHANNELLIST_URL: string;
     readonly VITE_YOUTUBE_CHANNEL_ICON_URL: string;
 }
