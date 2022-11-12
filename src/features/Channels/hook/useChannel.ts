825d2395bb682eb6f56c92d83e8bc0af3b5dd74e
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-11-12 18:26:21 +0900

    変更 - コードを移植

---
+++ src/features/Channels/hook/useChannel.ts
@@ -0,0 +1,71 @@
+import { useEffect } from 'react';
+import { atom, DefaultValue, selector, useRecoilState } from 'recoil';
+import { HikasenVtuber } from '../types/index';
+import { get as axiosGet } from '../../../utility/axios';
+import { AxiosResut } from '../../../types/api/index';
+
+const ChannelsAtom = atom<HikasenVtuber[]>({
+    key: 'Channels-atom',
+    default: [],
+});
+
+const ResultStatusAtom = atom<Omit<AxiosResut<HikasenVtuber[]>, 'payload'>>({
+    key: 'AxiosResut-atom',
+    default: { status: 200 },
+});
+
+const ChannelsSelector = selector<HikasenVtuber[]>({
+    key: 'Channels-selector',
+    get: async ({ get }) => {
+        const channels = get(ChannelsAtom);
+        if (channels.length > 0) {
+            return channels;
+        } else {
+            const request = await axiosGet<HikasenVtuber[]>(
+                import.meta.env.VITE_CHANNELLIST_SPREADSHEET_URL
+            );
+
+            return request.payload;
+        }
+    },
+    set: ({ set }, newChannels) => {
+        if (newChannels instanceof DefaultValue) {
+            return newChannels;
+        } else {
+            set(ChannelsAtom, newChannels);
+        }
+    },
+});
+
+const ResponseResultSelector = selector<
+    Omit<AxiosResut<HikasenVtuber[]>, 'payload'>
+>({
+    key: 'GAS-Response-selector',
+    get: async ({ get }) => {
+        return get(ResultStatusAtom);
+    },
+    set: ({ set }, newStatus) => {
+        if (newStatus instanceof DefaultValue) {
+            return newStatus;
+        } else {
+            set(ResultStatusAtom, newStatus);
+        }
+    },
+});
+
+export const useChannels = () => {
+    const [channels, store] = useRecoilState(ChannelsSelector);
+    const [resultStatus, setresultStatus] = useRecoilState(
+        ResponseResultSelector
+    );
+
+    useEffect(() => {
+        store(channels);
+    }, []);
+
+    const reload = async (): Promise<void> => {
+        store([]);
+    };
+
+    return [channels, resultStatus, reload] as const;
+};
