ca8c10ca96ae43dd8d1f8c58ae06401c3c2eeadb
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-11-15 11:15:34 +0900

    変更 - 表示処理などはComponent内で行わず、要素の構成だけにしたいので、一部処理を削除

--- src/features/Channels/component/ChannelPanel/index.tsx
+++ src/features/Channels/component/ChannelPanel/index.tsx
@@ -18,61 +18,57 @@ export const ChannelPanel = ({ channels }: Props) => {
         理由：サイドリロードさせたいので
         */
     }
-    const timeOutError = () => {
-        return (
-            <div>
-                <h3>タイムアウトエラー</h3>
-                <button onClick={reload}>再度読み込む</button>
-            </div>
-        );
-    };
+    // const timeOutError = () => {
+    //     return (
+    //         <div>
+    //             <h3>タイムアウトエラー</h3>
+    //             <button onClick={reload}>再度読み込む</button>
+    //         </div>
+    //     );
+    // };
 
-    const success = () => {
-        return (
-            <ul className="flex justify-center flex-wrap ml-10 mr-10">
-                {channels.map((channel) => (
-                    <li
-                        key={channel.channelID}
-                        className="flex flex-col w-96 p-4 m-2 bg-gray-700 rounded-md">
-                        <div className="grid grid-row-3 grid-flow-col gap-4">
-                            <div className="row-span-3">
-                                <Link
-                                    to={`/Channel/${channel.channelID}`}
-                                    title={`${channel.name}のアーカイブ`}>
-                                    <IconElement
-                                        src={`${iconURL}${channel.channelIconID}`}
-                                        alt={`${channel.name}のチャンネルアイコン`}
-                                        size="Medium"
-                                        radius="full"
-                                    />
-                                </Link>
-                            </div>
-                            <div className="row-span-1 col-span-2 mt-4 flex flex-col inline-block justify-center items-center \">
-                                <Link
-                                    to={`/Channel/${channel.channelID}`}
-                                    title={`${channel.name}のアーカイブ`}>
-                                    <div className="flex flex-col">
-                                        <span className="text-gray-100 font-sans text-lg">
-                                            {channel.name}
-                                        </span>
-                                        <span className="text-gray-400 font-sans text-sm">
-                                            Channel Name
-                                        </span>
-                                    </div>
-                                </Link>
-                                <span className="text-gray-400 font-sans text-xs">
-                                    Since 2013/8/24
-                                </span>
-                            </div>
-                            <div className="row-span-2 col-span-2">
-                                TAGTAGTAGTAGATGA
-                            </div>
+    return (
+        <ul className="flex justify-center flex-wrap ml-10 mr-10">
+            {channels.map((channel) => (
+                <li
+                    key={channel.channelID}
+                    className="flex flex-col w-96 p-4 m-2 bg-gray-700 rounded-md">
+                    <div className="grid grid-row-3 grid-flow-col gap-4">
+                        <div className="row-span-3">
+                            <Link
+                                to={`/Channel/${channel.channelID}`}
+                                title={`${channel.name}のアーカイブ`}>
+                                <IconElement
+                                    src={`${iconURL}${channel.channelIconID}`}
+                                    alt={`${channel.name}のチャンネルアイコン`}
+                                    size="Medium"
+                                    radius="full"
+                                />
+                            </Link>
                         </div>
-                    </li>
-                ))}
-            </ul>
-        );
-    };
-
-    return <div>{isTimeOut ? timeOutError() : success()}</div>;
+                        <div className="row-span-1 col-span-2 mt-4 flex flex-col inline-block justify-center items-center \">
+                            <Link
+                                to={`/Channel/${channel.channelID}`}
+                                title={`${channel.name}のアーカイブ`}>
+                                <div className="flex flex-col">
+                                    <span className="text-gray-100 font-sans text-lg">
+                                        {channel.name}
+                                    </span>
+                                    <span className="text-gray-400 font-sans text-sm">
+                                        Channel Name
+                                    </span>
+                                </div>
+                            </Link>
+                            <span className="text-gray-400 font-sans text-xs">
+                                Since 2013/8/24
+                            </span>
+                        </div>
+                        <div className="row-span-2 col-span-2">
+                            TAGTAGTAGTAGATGA
+                        </div>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    );
 };
