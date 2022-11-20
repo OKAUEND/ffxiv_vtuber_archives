82e4b8aad0621ee96979a39b2e6db89d81383917
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-11-20 18:37:51 +0900

    変更 - 環境変数に型を付けてセーフティな運用をしたいため、その準備として設定ファイルを編集

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+
+/**
+ * @param {string} appEnv
+ */
+function loadEnv(appEnv = 'local') {
+    const env = {
+        ...require(`./env/env.${appEnv}`),
+        NEXT_PUBLIC_APP_ENV: appEnv,
+    };
+
+    Object.entries(env).forEach(([key, value]) => {
+        process.env[key] = value;
+    });
+}
+
+loadEnv(process.env.APP_ENV);
