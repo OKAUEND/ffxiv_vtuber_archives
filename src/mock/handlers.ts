eea7df1386bb2ad932d4b1d0684d1e70386ecf91
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-11-21 23:21:23 +0900

    変更 - API-Routerへのパスを指定し、mockを作ってみるテスト

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -1,1 +1,10 @@
 import { rest } from 'msw';
+
+export const handlers = [
+    rest.post('../api/channel', (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
+    rest.post('../api/archives', (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
+];
