69ce6002c76b82f8e53982931b5e4d2a94bf41a5
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-11-22 14:55:20 +0900

    変更 - mockの設定を書き込み、開発環境化ならそっちを使うように設定する

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import { setupWorker } from 'msw';
+
+if (process.env.NODE_ENV === 'development') {
+    if (typeof window === 'undefined') {
+        const handlers = require('@/src/mock/handlers');
+        const worker = setupWorker(...handlers);
+        worker.start();
+    }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
