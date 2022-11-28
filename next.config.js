f0515dad71e0ae73b3b3263409118c3c18d1f414
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-11-29 01:15:43 +0900

    変更 - 環境変数の読み込み方をNext.js対応のに変更する

--- next.config.js
+++ next.config.js
@@ -4,19 +4,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-/**
- * @param {string} appEnv
- */
-function loadEnv(appEnv = 'local') {
-    const env = {
-        ...require(`./env/env.${appEnv}`),
-        NEXT_PUBLIC_APP_ENV: appEnv,
-    };
-
-    Object.entries(env).forEach(([key, value]) => {
-        process.env[key] = value;
-    });
-}
-
-loadEnv(process.env.APP_ENV);
