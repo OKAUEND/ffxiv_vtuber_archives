83c8779a7730f7a428ab4d8218ab558da2f36d63
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-11-30 19:45:39 +0900

    環境 - tailwindの設定をする

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extends: {},
     },
