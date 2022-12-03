cfbf90bb64daa6ac095c26bcbeef24ee4883d6dc
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-12-04 04:16:40 +0900

    変更 - mockを参照する方法を変更

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,10 +1,9 @@
-import { server } from './server';
-import { worker } from './browser';
-
 async function initMock() {
     if (typeof window === 'undefined') {
+        const { server } = await import('./server');
         server.listen();
     } else {
+        const { worker } = await import('./browser');
         worker.start();
     }
 }
