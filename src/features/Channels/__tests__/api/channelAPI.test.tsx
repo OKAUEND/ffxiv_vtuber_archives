c8698e9378f0454b5fd3c79dbbca5c279ef66276
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-12-05 19:21:47 +0900

    変更 - APIRouterのテストに切り替えるため、一旦中身を削除

--- src/features/Channels/__tests__/api/channelAPI.test.tsx
+++ src/features/Channels/__tests__/api/channelAPI.test.tsx
@@ -1,13 +1,4 @@
-import axios from 'axios';
-import React from 'react';
-import { vi } from 'vitest';
-import { RenderResult, renderHook, act } from '@testing-library/react-hooks';
-import { RecoilRoot } from 'recoil';
-import { useChannels } from '../../api/getChannels';
-import * as AxiosInstanceModule from '../../../../utility/axios/index';
 import { HikasenVtuber } from '../../types/index';
-import { AxiosStatusFactory } from '../../../../utility/test/AxiosResult';
-import { waitFor } from '@testing-library/react';
 
 const HikasenVtuberResourceFactory = (name: string): HikasenVtuber => {
     return {
@@ -24,27 +15,5 @@ const HikasenVtuberResourceFactory = (name: string): HikasenVtuber => {
 };
 
 describe('Channel Get API TEST', () => {
-    test('リロードの関数を使用したら、取得関数がコールされるか', async () => {
-        const mock = vi
-            .spyOn(AxiosInstanceModule, 'get')
-            .mockImplementationOnce(() => {
-                return Promise.resolve(
-                    AxiosStatusFactory(200, true, [
-                        HikasenVtuberResourceFactory('mockTEST'),
-                    ])
-                );
-            });
-
-        const { result } = renderHook(() => useChannels(), {
-            wrapper: RecoilRoot,
-        });
-
-        await waitFor(async () => {
-            const [channels, resultStatus, loadData] = result.current;
-
-            loadData();
-
-            expect(mock).toHaveBeenCalledTimes(2);
-        });
-    });
+    test('APIをコールされた時にAPIRouterが値を返すか', async () => {});
 });
