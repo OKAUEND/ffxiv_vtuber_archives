4a43659e72f5a41e5b5a0086f21d28bec868ce5e
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-12-06 08:24:34 +0900

    変更 - ハンドラーで使用する型

--- src/features/Channels/mock/index.ts
+++ src/features/Channels/mock/index.ts
@@ -1,5 +1,15 @@
 import { rest } from 'msw';
 
+type Data = {
+    id: string;
+    title: string;
+    body: string;
+};
+
+type Error = {
+    message: string;
+    status: number;
+};
 export const channelPostHandler = (status: 200 | 400 = 200) => {
     rest.get();
 };
