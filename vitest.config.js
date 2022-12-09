096ed08147db3598931f4743e363d3ca97177666
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-12-09 19:26:36 +0900

    環境 - テストに必要なパッケージを導入し、基本設定を記述

--- vitest.config.js
+++ vitest.config.js
@@ -1,4 +1,12 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.join(__dirname, '/'),
+        },
+    },
 });
