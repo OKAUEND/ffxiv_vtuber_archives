22d5ba272c13b47f02b97718a006eeb90412524e
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-12-16 10:23:44 +0900

    変更 - 必要なパッケージやモジュールをインポート

--- src/features/Channels/__tests__/index.test.tsx
+++ src/features/Channels/__tests__/index.test.tsx
@@ -1,3 +1,14 @@
+import { testApiHandler } from 'next-test-api-route-handler';
+import { describe, expect, test } from 'vitest';
+
+import { channelPostHandler } from '@/src/features/Channels/mock';
+
+import { handler } from '@/src/features/Channels/api/channel';
+import { handlers } from '@/src/mock/handlers';
+import { setupMockServer } from '@/src/mock/test/setup';
+
+const server = setupMockServer(handlers);
+
 describe('Channel - getServerSideProps', () => {
     test('200 - 成功時', () => {});
     test('400 - 失敗時', () => {});
