e0689aed755a6fe46d729daf2faf8f1d2846d38c
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-12-19 10:53:51 +0900

    変更 - テスト時に渡すContextを作成

--- src/features/Channels/__tests__/index.test.tsx
+++ src/features/Channels/__tests__/index.test.tsx
@@ -1,5 +1,7 @@
 import { describe, expect, test } from 'vitest';
 import { channelPostHandler } from '@/src/features/Channels/mock';
+import { createRequest, createResponse } from 'node-mocks-http';
+import { GetServerSidePropsContext } from 'next';
 
 import { handler } from '@/src/features/Channels/api/channel';
 import { handlers } from '@/src/mock/handlers';
@@ -8,7 +10,15 @@ import { setupMockServer } from '@/src/mock/test/setup';
 const server = setupMockServer(handlers);
 
 describe('Channel - getServerSideProps', () => {
-    test('200 - 成功時', () => {});
+    const mockCtx = (): GetServerSidePropsContext => ({
+        req: createRequest(),
+        res: createResponse(),
+        params: undefined,
+        query: {},
+        resolvedUrl: '',
+    });
+
+    test('200 - 成功時', async () => {});
     test('400 - 失敗時', () => {});
     test('500 - 失敗時', () => {});
 });
