922e7b50a0c422c0db74f457b610d9c930b32f65
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-12-19 12:27:11 +0900

    変更 - テストの評価を行う

--- src/features/Channels/__tests__/index.test.tsx
+++ src/features/Channels/__tests__/index.test.tsx
@@ -7,6 +7,9 @@ import { handler } from '@/src/features/Channels/api/channel';
 import { handlers } from '@/src/mock/handlers';
 import { setupMockServer } from '@/src/mock/test/setup';
 
+import { getServerSideProps } from '../../../../pages/';
+import { HikasenVtuberResourceFactory } from '@/src/features/Channels/mock';
+
 const server = setupMockServer(handlers);
 
 describe('Channel - getServerSideProps', () => {
@@ -27,7 +30,12 @@ describe('Channel - getServerSideProps', () => {
         if (typeof (item as any).props != 'object') err();
     }
 
-    test('200 - 成功時', async () => {});
+    test('200 - 成功時', async () => {
+        const res = await getServerSideProps(mockCtx());
+        assertHasProps(res);
+        const channel = res.props['channels']['data'];
+        expect(channel).toStrictEqual([HikasenVtuberResourceFactory('Mock')]);
+    });
     test('400 - 失敗時', () => {});
     test('500 - 失敗時', () => {});
 });
