8a1c447e878a26db937b77e5209acbff42444d45
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-12-21 18:36:23 +0900

    変更 - Factory関数を移動

---
+++ src/features/Archives/mock/index.ts
@@ -0,0 +1,83 @@
+export const GoogleYoutubeFactory = (
+    name: string
+): GoogleApiYouTubePageInfo<GoogleApiYouTubeSearchResource> => {
+    return {
+        kind: name,
+        etag: name,
+        items: [
+            {
+                kind: name,
+                etag: name,
+                id: {
+                    kind: name,
+                    videoId: name,
+                    channelId: name,
+                    playlistId: name,
+                },
+                snippet: {
+                    publishedAt: name,
+                    channelId: name,
+                    title: name,
+                    description: name,
+                    thumbnails: {
+                        default: {
+                            url: name,
+                            width: 99,
+                            height: 99,
+                        },
+                        high: {
+                            url: name,
+                            width: 99,
+                            height: 99,
+                        },
+                        medium: {
+                            url: name,
+                            width: 99,
+                            height: 99,
+                        },
+                    },
+                    channelTitle: name,
+                },
+            },
+        ],
+    };
+};
+
+export const YoutubeResourceFactory = (
+    name: string
+): GoogleApiYouTubeSearchResource => {
+    return {
+        kind: name,
+        etag: name,
+        id: {
+            kind: name,
+            videoId: name,
+            channelId: name,
+            playlistId: name,
+        },
+        snippet: {
+            publishedAt: name,
+            channelId: name,
+            title: name,
+            description: name,
+            thumbnails: {
+                default: {
+                    url: name,
+                    width: 99,
+                    height: 99,
+                },
+                high: {
+                    url: name,
+                    width: 99,
+                    height: 99,
+                },
+                medium: {
+                    url: name,
+                    width: 99,
+                    height: 99,
+                },
+            },
+            channelTitle: name,
+        },
+    };
+};
