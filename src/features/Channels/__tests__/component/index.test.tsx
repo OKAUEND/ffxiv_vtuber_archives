24cacb3fa3843044d5ae41787f214426565d6b3f
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-12-27 14:09:11 +0900

    変更 - テスト用データを作成する

--- src/features/Channels/__tests__/component/index.test.tsx
+++ src/features/Channels/__tests__/component/index.test.tsx
@@ -4,8 +4,20 @@ import {} from '@testing-library/jest-dom';
 import {} from 'recoil';
 
 import Home from '../../../../../pages/index';
+import { HikasenVtuberResourceFactory } from '@/src/features/Channels/mock';
+import { Data } from '@/src/types/api';
+import { HikasenVtuber } from '../../types';
 
 describe('Channel Component TEST', () => {
+    const List = HikasenVtuberResourceFactory('ComponentTEST');
+    type Props = Data<HikasenVtuber>;
+
+    const Data: Props = {
+        status: 200,
+        message: 'Success!',
+        item: [List],
+    };
     test('Propsが渡され、正しくその内容が表示されているか', async () => {});
+
     test('Propsでエラーが渡された場合、エラー画面が表示されるか', async () => {});
 });
