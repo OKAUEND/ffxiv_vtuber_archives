994659c91d9fa8ed618808d4921e7e07bde1d1a8
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-12-27 10:50:53 +0900

    変更 - テスト項目を作成

--- src/features/Channels/__tests__/component/index.test.tsx
+++ src/features/Channels/__tests__/component/index.test.tsx
@@ -1,3 +1,8 @@
-import { vi } from 'vitest';
+import { vi, describe, test } from 'vitest';
 import {} from '@testing-library/react';
 import {} from '@testing-library/jest-dom';
+
+describe('Channel Component TEST', () => {
+    test('Propsが渡され、正しくその内容が表示されているか', async () => {});
+    test('Propsでエラーが渡された場合、エラー画面が表示されるか', async () => {});
+});
