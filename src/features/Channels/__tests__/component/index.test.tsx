3825ceb481b5005acebce259cdceb553d0b32d60
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-12-27 10:54:37 +0900

    変更 - テスト対象のコンポーネントをインポート

--- src/features/Channels/__tests__/component/index.test.tsx
+++ src/features/Channels/__tests__/component/index.test.tsx
@@ -2,6 +2,8 @@ import { vi, describe, test } from 'vitest';
 import {} from '@testing-library/react';
 import {} from '@testing-library/jest-dom';
 
+import Home from '../../../../../pages/index';
+
 describe('Channel Component TEST', () => {
     test('Propsが渡され、正しくその内容が表示されているか', async () => {});
     test('Propsでエラーが渡された場合、エラー画面が表示されるか', async () => {});
