d57240e714ba98d950127ad058f788f82eb09b5c
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-12-29 18:52:56 +0900

    変更 - エラーステータスと、エラーメッセージを表示し、ユーザーに操作を求める文を表記

--- src/component/Error/index.tsx
+++ src/component/Error/index.tsx
@@ -1,3 +1,19 @@
-export const Error = () => {
-    return <div></div>;
+export const Error = ({
+    status,
+    message,
+}: {
+    status: number;
+    message: string;
+}) => {
+    return (
+        <div>
+            <h1>{status}</h1>
+            <div>
+                <p>{message}</p>
+                <p>
+                    エラーが発生しました。再度操作を最初からやり直してください。
+                </p>
+            </div>
+        </div>
+    );
 };
