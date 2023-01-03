b57bfbde1a742a6238fecfa3e593bd041b71793e
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-01-03 16:45:43 +0900

    変更 - テスト要件を作成

--- src/features/Archives/__tests__/hooks/useArchive.test.ts
+++ src/features/Archives/__tests__/hooks/useArchive.test.ts
@@ -1,4 +1,10 @@
 import { describe, test } from 'vitest';
 import {} from 'msw';
 
-describe('useArchive TEST', () => {});
+describe('useArchive TEST', () => {
+    test('CustomHooksを生成した時に、Fetchで取得した値をStateにいれタプルでHooksの外に出せているか', () => {});
+    test('CustomHooksを生成した時に通信エラー発生した場合に、Errorの値があるか', () => {});
+    test('次の値を取得する更新関数を呼び出した場合、値を取得し、Stateへセットできるか', () => {});
+    test('次の値を取得する更新関数を呼び出した場合、通信エラー発生した場合に、Errorの値があるか', () => {});
+    test('次の値を取得する更新関数を呼び出した場合、通信エラー発生した場合で、キャッシュがある場合でもErrorの値があるか', () => {});
+});
