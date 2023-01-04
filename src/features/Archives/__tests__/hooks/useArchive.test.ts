a2ede416a1a6b271c6307d861c06032996e394eb
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-01-04 20:22:24 +0900

    変更 - 成功時のテストを書く

--- src/features/Archives/__tests__/hooks/useArchive.test.ts
+++ src/features/Archives/__tests__/hooks/useArchive.test.ts
@@ -1,8 +1,18 @@
-import { describe, test } from 'vitest';
+import { describe, test, expect } from 'vitest';
 import {} from 'msw';
+import { renderHook, waitFor } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
+
+import { useArchives } from '@/src/features/Archives/hook/useArchive';
 
 describe('useArchive TEST', () => {
-    test('CustomHooksを生成した時に、Fetchで取得した値をStateにいれタプルでHooksの外に出せているか', () => {});
+    test('CustomHooksを生成した時に、Fetchで取得した値をStateにいれタプルでHooksの外に出せているか', async () => {
+        const result = renderHook(useArchives, { wrapper: RecoilRoot });
+
+        await waitFor(() => {
+            expect(result[0]).toStrictEqual({});
+        });
+    });
     test('CustomHooksを生成した時に通信エラー発生した場合に、Errorの値があるか', () => {});
     test('次の値を取得する更新関数を呼び出した場合、値を取得し、Stateへセットできるか', () => {});
     test('次の値を取得する更新関数を呼び出した場合、通信エラー発生した場合に、Errorの値があるか', () => {});
