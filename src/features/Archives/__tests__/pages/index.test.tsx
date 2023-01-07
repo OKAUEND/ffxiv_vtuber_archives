6360721b0c3dc19694a94de6a49a6ef4cda676d5
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-01-07 23:53:55 +0900

    新規 - Pagesのコンポーネントテスト用のファイルを作成

---
+++ src/features/Archives/__tests__/pages/index.test.tsx
@@ -0,0 +1,6 @@
+import { vi, describe, test, expect } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import {} from '@testing-library/jest-dom';
+import { RecoilRoot } from 'recoil';
+
+describe('Archives Component TEST', () => {});
