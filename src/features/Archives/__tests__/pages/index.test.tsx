f6073aa52f44f1be39607eb367e5fb2f0bf9f0ff
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-01-08 01:30:56 +0900

    変更 - とりあえずのテスト内容を決める(後日追加予定

--- src/features/Archives/__tests__/pages/index.test.tsx
+++ src/features/Archives/__tests__/pages/index.test.tsx
@@ -3,4 +3,12 @@ import { render, screen, waitFor } from '@testing-library/react';
 import {} from '@testing-library/jest-dom';
 import { RecoilRoot } from 'recoil';
 
-describe('Archives Component TEST', () => {});
+describe('Archives Component TEST', () => {
+    test('Hookから値から渡されたデータを子に渡し、要素が表示できているか');
+    test(
+        '初期インスタンス生成時に通信エラーが発生した時、子の要素は表示されずエラーコンポーネントが表示されるか'
+    );
+    test(
+        '次の取得が選ばれた時、APIより新しい値を取得し、要素へ反映できているか'
+    );
+});
