f4e9065624d3b86ddf6d5d3f842c727c83196447
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-01-30 21:10:32 +0900

    変更 - v3からPugeがいらなくなったので、公式ドキュメントを参考にコンテンツを指定しなおす

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     mode: 'jit',
-    purge: {
-        content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
-    },
+    content: [
+        './app/**/*.{js,ts,jsx,tsx}',
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+
+        // Or if using `src` directory:
+        './src/**/*.{js,ts,jsx,tsx}',
+    ],
 
     theme: {
         extends: {
