d3288e3592ca5bd210804ea745eeeb9c0fbd3163
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-01-31 20:34:12 +0900

    変更 - headerがないとmain要素が潰れてしまうので、header要素を追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,8 @@ export default function Home({ status, message, item, error }: Props) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
+            <header>Header</header>
+
             <main className="bg-gray-800 h-full">
                 <div>
                     <Suspense fallback={<p>Loading...</p>}>
