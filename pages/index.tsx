85f154a2d5a3c15745f5322e1842fc46f6cbcd66
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-02-01 20:40:31 +0900

    削除 - デフォルトのCSSファイルを削除

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 import { Channels, HikasenVtuber } from '@/src/features/Channels';
 import { Suspense, useEffect } from 'react';
