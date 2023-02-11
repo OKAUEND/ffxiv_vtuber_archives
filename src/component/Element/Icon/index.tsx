36bf1fc1780413d6f89d776582d7606dadd93849
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-02-11 20:32:11 +0900

    変更 - アイコンを表示するための共通コンポーネントを、NextImageを使うように内容修正

--- src/component/Element/Icon/index.tsx
+++ src/component/Element/Icon/index.tsx
@@ -1,41 +1,12 @@
-import React from 'react';
+import Image from 'next/image';
 
-//アイコンのサイズをPropsで指定したいので型の内容を制限する
-type size = 'Large' | 'Medium' | 'Small';
-
-type radius = 'none' | 'full';
-
-type ImageProps = JSX.IntrinsicElements['img'];
-
-type Props = ImageProps & {
-    size: size;
-    radius: radius;
+type Props = {
+    src: string;
+    alt: string;
+    width: number;
+    heidht: number;
 };
 
-export const IconElement = (props: Props): JSX.Element => {
-    const elementSize = () => {
-        switch (props.size) {
-            case 'Large':
-                return 'w-40 h-40';
-            case 'Medium':
-                return 'w-28 h-28';
-            case 'Small':
-                return 'w-16 h-16';
-        }
-    };
-
-    const elementRadius = () => {
-        switch (props.radius) {
-            case 'none':
-                return 'rounded-none';
-            case 'full':
-                return 'rounded-full';
-        }
-    };
-
-    return (
-        <div className={`overflow-hidden ${elementRadius()} ${elementSize()}`}>
-            <img {...props} />
-        </div>
-    );
+export const Icon = (props: Props): JSX.Element => {
+    return <Image src={props.src} alt={props.alt} width={48} height={48} />;
 };
