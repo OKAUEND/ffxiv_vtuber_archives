e9581cbb277dd29f0efebdf2df742ea5ecb6daed
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-02-15 00:27:22 +0900

    変更 - Propsで値を渡された時の挙動を確認するテストを作成

--- src/features/Channels/__tests__/component/channelPanel/index.test.tsx
+++ src/features/Channels/__tests__/component/channelPanel/index.test.tsx
@@ -3,5 +3,9 @@ import { ChannelPanel } from '@/src/features/Channels/component/ChannelPanel';
 import { HikasenVtuberResourceFactory } from '@/src/features/Channels/mock';
 import { render, screen } from '@testing-library/react';
 
+describe('ChannelPanel Component TEST', () => {
+    test('DataがPropsで渡された場合、表示されているか', () => {});
+});
+
 
 describe('ChannelPanel Component TEST', () => {});
