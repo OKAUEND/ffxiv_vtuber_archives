c60cf983cadd0f7f4c1ffab97a5db2576eb8f534
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-03-25 19:35:10 +0900

    変更 - プリフィクスのエラーが発生するため、設定を削除する

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : '';
 
 const nextConfig = {
     reactStrictMode: true,
-    assetPrefix: urlPrefix,
     basePath: urlPrefix,
     trailingSlash: true,
     publicRuntimeConfig: { urlPrefix },
