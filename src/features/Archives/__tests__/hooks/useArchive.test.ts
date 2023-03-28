97c88c40f41f6c16f61307a86553bbdf2d77b006
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-03-28 13:04:31 +0900

    変更 - 新しく作ったCustomHookのテスト項目を作成

--- src/features/Archives/__tests__/hooks/useArchive.test.ts
+++ src/features/Archives/__tests__/hooks/useArchive.test.ts
@@ -17,5 +17,11 @@ const server = setupMockServer(handlers);
 describe('useArchive TEST', () => {
     const mockYoutubeData = GoogleYoutubeFactory();
     test('初期取得で値を取得できているか', () => {});
-    test('初期取得で値を取得できているか', () => {});
+    test('エラー発生時の対応(- エラー発生時の動作がまだ未定なので仮に項目作成)', () => {});
+});
+
+describe('usePage TEST', () => {
+    const mockYoutubeData = GoogleYoutubeFactory();
+    test('初期値は事前に設定している値になっているか', () => {});
+    test('更新関数を呼び出した時、値がインクリメントされているか', () => {});
 });
