ca16ab42421f754b3a4304960bce0b6bf482817a
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-04-08 23:34:47 +0900

    変更 - テスト項目を実装し、必要なモジュールやライブラリを読み込む

---
+++ src/hooks/error/__tests__/useError.test.ts
@@ -0,0 +1,12 @@
+import { act, renderHook } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
+import { useError, errorState, SetErrorData } from '../useError';
+
+describe('useError', () => {
+    it('useErrorの戻値で、それぞれError状態、関数、関数のタプルになっているか', () => {});
+
+    it('エラー状態をセットできるか', () => {});
+
+    it('エラー状態をリセットできるか', () => {});
+});
+
