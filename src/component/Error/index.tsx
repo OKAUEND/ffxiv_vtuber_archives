fb165fd67ca6c92fe96e1d2f806d79536ef61f4f
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-04-09 10:42:42 +0900

    変更 - hookからの値を参照するように変更

--- src/component/Error/index.tsx
+++ src/component/Error/index.tsx
@@ -10,9 +10,9 @@ export const Error = () => {
 
     return (
         <div>
-            <h1>{status}</h1>
+            <h1>{error.status}</h1>
             <div>
-                <p>{message}</p>
+                <p>{error.message}</p>
                 <p>
                     エラーが発生しました。再度操作を最初からやり直してください。
                 </p>
