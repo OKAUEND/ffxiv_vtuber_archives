6a4b2b81462d3455130b57a4ea5267c0b4166ea4
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-04-10 20:43:46 +0900

    新規 - エラーコンポーネントをテストするUnit Testを行う

---
+++ src/component/Error/__tests__/ErrorNotification.test.tsx
@@ -0,0 +1,12 @@
+import {} from '../index';
+import { vi, describe, test, expect, vitest, Vitest } from 'vitest';
+import { act, render, screen } from '@testing-library/react';
+import {} from '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import { RecoilRoot } from 'recoil';
+
+import {} from '../index';
+
+describe('ErrorNotification Unit TEST', () => {
+    test('エラー状態出ない場合はエラー画面が表示されていないか');
+});
