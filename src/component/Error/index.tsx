537230d4861d17eafac5db8a1050ac6b8c8e716e
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-04-10 20:31:12 +0900

    変更 - エラー画面ではただエラーの事を通知するのではなく、操作を残す事でユーザーの混乱を減らす

--- src/component/Error/index.tsx
+++ src/component/Error/index.tsx
@@ -1,12 +1,25 @@
+import { useRouter } from 'next/router';
 import { useError } from '@/src/hooks/error/useError';
 
-export const ErrorNotification = () => {
+export const ErrorNotification = (handleReloadData: () => void) => {
     const [error, _, reset] = useError();
+    const router = useRouter();
 
     //エラーが発生していない状態で表示しないようにしておく
     if (!error.hasError) {
         return;
     }
+
+    //ユーザーがリロードを選択した時は、エラー状態をリセットし、尚且つ親から渡された再度アクセスを行う関数を呼び出す。
+    const handleResetError = () => {
+        handleReloadData();
+        reset();
+    };
+
+    //一つ前へ状態を維持しながら戻る実装が難しいため、問答無用でトップページへ戻るようにする
+    const handleClickReturnTop = () => {
+        router.push('/');
+    };
 
     return (
         <div>
@@ -16,6 +29,18 @@ export const ErrorNotification = () => {
                 <p>
                     エラーが発生しました。再度操作を最初からやり直してください。
                 </p>
+                <button
+                    onClick={() => {
+                        handleResetError();
+                    }}>
+                    リロード
+                </button>
+                <button
+                    onClick={() => {
+                        handleClickReturnTop();
+                    }}>
+                    トップページに戻る
+                </button>
             </div>
         </div>
     );
