8b42985180661bdb5b68dc45e736637f7f2a8f18
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-04-15 18:04:00 +0900

    変更 - テストでエラーとして扱うステータスをよくある3つに絞っておく

--- src/hooks/useFetch/__tests__/useFetch.test.ts
+++ src/hooks/useFetch/__tests__/useFetch.test.ts
@@ -2,4 +2,13 @@ import {} from 'vitest';
 import { act, renderHook } from '@testing-library/react';
 import { setupMockServer } from '@/src/mock/test/setup';
 
-describe('useFetch Unit Test', () => {});
+import { handlers } from '@/src/mock/handlers';
+
+const server = setupMockServer(handlers);
+
+describe('useFetch Unit Test', () => {
+    test('通信成功:200', () => {});
+    test('通信エラー:400', () => {});
+    test('通信エラー:404', () => {});
+    test('通信エラー:500', () => {});
+});
--- src/hooks/useFetch/mock/handles.ts
+++ src/hooks/useFetch/mock/handles.ts
@@ -2,19 +2,20 @@ import { rest } from 'msw';
 
 const path = () => 'http://fetch.api.mock/';
 
-export const useFetchAPIHandler = (status: 200 | 400 | 500 = 200) => {
+export const useFetchAPIHandler = (status: 200 | 400 | 404 | 500 = 200) => {
     return rest.get(path(), async (req, res, ctx) => {
         if (status === 400) {
-            return res(
-                ctx.status(400),
-                ctx.json({ message: 'Bad Request', status: 400 })
-            );
+            return res(ctx.status(400), ctx.json({ message: 'Request Error' }));
+        }
+
+        if (status === 404) {
+            return res(ctx.status(400), ctx.json({ message: 'Not Found' }));
         }
 
         if (status === 500) {
             return res(
                 ctx.status(status),
-                ctx.json({ message: 'Internal Server Error', status: 500 })
+                ctx.json({ message: 'Server Error' })
             );
         }
 
