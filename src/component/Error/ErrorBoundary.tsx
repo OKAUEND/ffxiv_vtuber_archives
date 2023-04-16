0232c643a963c09280db5be976963321678b58e2
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-04-16 20:31:40 +0900

    変更 - ErrorBoundaryを、公式ドキュメントを参考にコーディング

---
+++ src/component/Error/ErrorBoundary.tsx
@@ -0,0 +1,34 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+
+interface Props {
+    children: ReactNode;
+}
+
+interface State {
+    hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStatedFromError(_) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, info: ErrorInfo) {
+        console.error(error, info);
+    }
+
+    render(): React.ReactNode {
+        if (this.state.hasError) {
+            return <div>Error Mode</div>;
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundary;
