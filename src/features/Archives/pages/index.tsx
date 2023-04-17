7c89e59641e417617115c08bbc49200d91a939e2
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-04-17 17:45:25 +0900

    変更 - 不要になった処理を削除

--- src/features/Archives/pages/index.tsx
+++ src/features/Archives/pages/index.tsx
@@ -4,8 +4,6 @@ import ErrorBoundary from '@/src/component/Error/ErrorBoundary';
 import { Archive } from '@/src/features/Archives/pages/Archive';
 
 export const ArchiveRouter = () => {
-    // if (Archives.length === 0 && error)
-    //     return <Error status={error.status} message={error.message} />;
     return (
         <div className="w-full md:w-1/2">
             <ErrorBoundary>
