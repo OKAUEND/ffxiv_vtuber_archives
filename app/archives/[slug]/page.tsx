8e7d56c35d0aa85a1d555c88ed73745fd81a0e55
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-04-29 16:46:37 +0900

    変更 - Suspenseのローディングを追加しておく

--- app/archives/[slug]/page.tsx
+++ app/archives/[slug]/page.tsx
@@ -6,7 +6,7 @@ export default function Article({ params }: { params: { slug: string } }) {
     <div>
       <h1>テスト</h1>
       <p>パスID: {params.slug}</p>
-      <Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
         <TestFe />
       </Suspense>
     </div>
