3df2b2370a30c26a42833efe56e41634bb66d236
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-04-30 17:01:17 +0900

    変更 - エラーコンポーネントをセットする

--- app/archives/[slug]/page.tsx
+++ app/archives/[slug]/page.tsx
@@ -1,15 +1,18 @@
 import { TestFe } from '@/src/features/TestFe';
 import { Suspense } from 'react';
 import Loading from './loading';
+import Error from './error';
 
 export default function Article({ params }: { params: { slug: string } }) {
   return (
     <div>
       <h1>テスト</h1>
       <p>パスID: {params.slug}</p>
-      <Suspense fallback={<Loading />}>
-        <TestFe />
-      </Suspense>
+      <Error>
+        <Suspense fallback={<Loading />}>
+          <TestFe />
+        </Suspense>
+      </Error>
     </div>
   );
 }
