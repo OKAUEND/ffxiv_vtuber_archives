f6d660a9c834f4c2b6a383ced8cd4ff9d9b74911
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-04-30 15:35:40 +0900

    新規 - ローディングのUIを実装する

--- app/archives/[slug]/page.tsx
+++ app/archives/[slug]/page.tsx
@@ -1,12 +1,13 @@
 import { TestFe } from '@/src/features/TestFe';
 import { Suspense } from 'react';
+import Loading from './loading';
 
 export default function Article({ params }: { params: { slug: string } }) {
   return (
     <div>
       <h1>テスト</h1>
       <p>パスID: {params.slug}</p>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Loading />}>
         <TestFe />
       </Suspense>
     </div>
---
+++ src/component/Loading/Loading.tsx
@@ -0,0 +1,5 @@
+export const LoadingBasicAnimation = () => {
+  return (
+    <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+  );
+};
