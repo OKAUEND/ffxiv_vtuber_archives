cec3bea9b7c2a0caf1b1b9465847713982358466
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-15 23:12:07 +0900

    環境 - 13.4に上げ、安定版を動かせるようにする

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: 'jit',
-    content: [
-        './app/**/*.{js,ts,jsx,tsx}',
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-
-        // Or if using `src` directory:
-        './src/**/*.{js,ts,jsx,tsx}',
-    ],
-
-    theme: {
-        extend: {
-            gridTemplateColumns: {
-                main: 'auto 80% auto',
-            },
-            gridTemplateRows: {
-                footer: 'auto 1fr auto',
-            },
-        },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
-    plugins: [require('@tailwindcss/line-clamp')],
+  },
+  plugins: [],
 };
