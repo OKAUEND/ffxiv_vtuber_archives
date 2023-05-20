028e83f3b467b95785a11d48db25d1accc079aa5
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-20 13:57:24 +0900

    新規 - ページネーションの表示が想定通りかをテストする

---
+++ app/_components/Pagination/__tests__/Pagination.test.tsx
@@ -0,0 +1,9 @@
+import { vi, describe, test } from 'vitest';
+import { render } from '@testing-library/react';
+
+describe('Pagination Component TEST', () => {
+  test('現在ページが1だった場合');
+  test('現在ページが4だった場合');
+  test('現在ページが5だった場合');
+  test('現在ページが10だった場合');
+});
