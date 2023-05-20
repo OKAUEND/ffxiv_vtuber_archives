e6cf9aefcecf79aa741cd347954af3c26beaed44
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-20 20:52:51 +0900

    変更 - 実際の値を取得する

--- app/channels/_lib/api/getChannel.ts
+++ app/channels/_lib/api/getChannel.ts
@@ -1,17 +1,15 @@
-export const getChannel = async (offset: string) => {
+import { HikasenVtuber } from '@/src/types';
+
+export const getChannel = async (offset: string): Promise<HikasenVtuber[]> => {
   const BASE_QUERY_COUNT = 20;
   const query =
     Number(offset) === 1
       ? ''
       : `?offset=${BASE_QUERY_COUNT * (Number(offset) - 1)}&limit=20`;
-  const URL = `https://pokeapi.co/api/v2/pokemon/${query}`;
-  const res = await fetch(URL);
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
 
-  // Recommendation: handle errors
+  const URL = process.env.NEXT_PUBLIC_CHANNELLIST_URL;
+  const res = await fetch(URL);
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data');
   }
 
