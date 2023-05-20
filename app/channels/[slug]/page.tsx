8af30521b54df172ae007f775427ba4601709ab9
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-20 16:29:32 +0900

    新規 - 配信者チャンネル関連のページルートファイルを作成

---
+++ app/channels/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import { TestFe } from '@/src/features/TestFe';
+import { Suspense } from 'react';
+// import Error from './error';
+export default async function Article({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  return <div>TEST Channel!</div>;
+}
