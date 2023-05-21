6655d78480381a30c8c361917d87642f28b06bd5
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-21 15:07:30 +0900

    変更 - テスト構造:RouteHandlerを使えるか確かめるために、仮組みで組んでみる

--- app/archives/[channelID]/page.tsx
+++ app/archives/[channelID]/page.tsx
@@ -1,12 +1,20 @@
+'use client';
+import { use, cache } from 'react';
 import { Suspense } from 'react';
 import Loading from './loading';
 // import Error from './error';
+
+const getAPI = cache(async (channelID: string) => {
+  const res = await fetch(`/api/archives/${channelID}?mock=supermock`);
+  const data = await res.json();
+});
 
 export default async function Article({
   params,
 }: {
   params: { channelID: string };
 }) {
+  getAPI(params.channelID);
   return (
     <div>
       <h1>テスト</h1>
