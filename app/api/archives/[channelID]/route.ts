30471965357d5a17bf4c2163bb69571ddbf17013
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-22 16:04:41 +0900

    変更 - Route HandlerをAPI Routerの内容を元に作る

--- app/api/archives/[channelID]/route.ts
+++ app/api/archives/[channelID]/route.ts
@@ -1,4 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
+import { fetchExtend } from '@/app/_utile/fetch';
 
 export async function GET(
   request: NextRequest,
@@ -8,9 +9,18 @@ export async function GET(
     params: { slug: string };
   }
 ) {
-  console.log(request.nextUrl.searchParams.get('mock'));
-  console.log({ params });
-  const data = 'TEST';
+  const APIKey = process.env.API_KEY;
+  const query = request.nextUrl.searchParams.get('mock');
+  const slug = params.slug;
 
-  return NextResponse.json({ data });
+  const url = `${process.env.YOUTUBE_API_URL}?channelId=${slug}&key=${APIKey}&part=snippet&type=video&order=date&q=FF14|FFXIV&maxResults=25`;
+
+  if (slug === '' || slug === null) {
+    return new Response(JSON.stringify('TEST'), {
+      status: 400,
+    });
+  }
+  const res = await fetchExtend<
+    GoogleApiYouTubePaginationInfo<GoogleApiYouTubeSearchResource>
+  >({ url: url });
 }
