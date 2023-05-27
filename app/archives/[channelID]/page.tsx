ee84dd6229b0bd0d2488cc3d3b499f281c82b6e8
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-27 19:24:17 +0900

    変更 - ローディング中の画面表示を指定する

--- app/archives/[channelID]/page.tsx
+++ app/archives/[channelID]/page.tsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic';
 
 const DynamicArchiveClientComponent = dynamic(
   () => import('@/app/archives/_components/Archives/route'),
-  { ssr: false }
+  { ssr: false, loading: () => <div>Loading...</div> }
 );
 
 export default async function Article({
