a31db4d2663af8e31b57b3c4c1fdc68fc43bd6fe
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-27 22:02:34 +0900

    変更 - APIより取得したデータをリストで表示させる

--- app/archives/_components/index.tsx
+++ app/archives/_components/index.tsx
@@ -1,14 +1,19 @@
 'use client';
 
 import { useArchives } from '@/app/archives/(hooks)/useArchives';
+import { ArchiveList } from '@/app/archives/_components/Archives';
 
 interface Props {
   channelID: string;
 }
 
 const Archives = ({ channelID }: Props) => {
-  const channels = useArchives(channelID);
-  return <div>TEST</div>;
+  const { archives } = useArchives(channelID);
+  return (
+    <div>
+      <ArchiveList Archives={[...archives.archives]} />
+    </div>
+  );
 };
 
 export default Archives;
