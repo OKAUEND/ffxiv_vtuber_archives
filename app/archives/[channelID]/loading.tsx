7f9de5d94085baf14a872d2958aa7e99e6f8e8f6
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-28 19:45:20 +0900

    変更 - パスをApp Routeのに変更する

--- app/archives/[channelID]/loading.tsx
+++ app/archives/[channelID]/loading.tsx
@@ -1,4 +1,4 @@
-import { LoadingBasicAnimation } from '@/src/component/Loading';
+import { LoadingBasicAnimation } from '@/app/_components/Loading';
 
 export default function Loading() {
   return <LoadingBasicAnimation />;
