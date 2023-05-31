608c29cff638bddb6f9a595b3a47c55a35c5bc5d
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-31 21:02:47 +0900

    削除 - 旧版のファイルを削除する

--- src/component/Loading/Loading.tsx
+++
@@ -1,5 +0,0 @@
-export const LoadingBasicAnimation = () => {
-  return (
-    <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
-  );
-};
--- src/features/Channels/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './pages';
-
-export * from './api/channel';
-
-export * from './types';
