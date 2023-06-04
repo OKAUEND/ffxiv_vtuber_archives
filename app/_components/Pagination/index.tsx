ebf6dd5bd2fdb6f9ed888a3ef08af8fb706b8104
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-05 01:47:26 +0900

    変更 - 現在ページは赤の縁がつくようにロジックを追加

--- app/_components/Pagination/index.tsx
+++ app/_components/Pagination/index.tsx
@@ -2,6 +2,8 @@
 
 import Link from 'next/link';
 
+import styles from './pagination.module.scss';
+
 interface Props {
   // totalCount: number;
   basePath: string;
@@ -16,10 +18,20 @@ export const Pagination = ({ basePath, currentPageNumber = 1 }: Props) => {
   const paginationList = () =>
     [...Array(PER_PAGE)].map((_, index) => offsetPageNumber + index);
 
+  const toggleCurrantNumberStyle = (
+    currantNumber: number,
+    pageNumber: number
+  ) => {
+    return currantNumber === pageNumber ? styles.curreant : styles.pagination;
+  };
+
   return (
-    <ul>
+    <ul className={styles.container}>
       {paginationList().map((number, index) => (
-        <li key={index}>
+        <li
+          className={toggleCurrantNumberStyle(currentPageNumber, number)}
+          key={index}
+        >
           <Link href={`${basePath}/${number}`}>{number}</Link>
         </li>
       ))}
