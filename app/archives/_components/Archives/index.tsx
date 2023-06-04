07fa729ce602d57e638105e184a4b70dd0ba5fa4
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-04 20:46:30 +0900

    変更 - モバイルファーストのスタイリングをする また、リスト要素に空白が入っていたので0に指定

--- app/archives/_components/Archives/index.tsx
+++ app/archives/_components/Archives/index.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import styles from '@/archives/_styles/archives.module.scss';
+
 interface IProps {
   Archives: GoogleApiYouTubeSearchResource[];
 }
@@ -15,9 +17,9 @@ export const ArchiveList = ({ Archives }: IProps) => {
   };
 
   return (
-    <ul className="list-none w-full">
+    <ul className={styles.container}>
       {Archives.map((archive, index) => (
-        <li key={index} className="flex flex-row box-border p-1 md:p-3">
+        <li key={index} className={styles.panel}>
           <div className="w-1/2 h-1/2 md:w-160 md:h-90 flex-shrink-0">
             <a
               href={`https://www.youtube.com/watch?v=${archive.id.videoId}`}
@@ -31,11 +33,9 @@ export const ArchiveList = ({ Archives }: IProps) => {
             </a>
           </div>
           <div className="flex-grow w-1/4 ml-2 text-gray-50">
-            <div className="flex flex-col  h-1/2 justify-center">
-              <p className="max-h-12 line-clamp-2 text-base md:text-2xl text-left truncate w-full">
-                {archive.snippet.title}
-              </p>
-            </div>
+            <p className="max-h-12 line-clamp-2 text-base md:text-2xl text-left truncate w-full">
+              {archive.snippet.title}
+            </p>
             <div className="text-sm h-1/2 text-gray-400">
               {archive.snippet.publishedAt}
             </div>
