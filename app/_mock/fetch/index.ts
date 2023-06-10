d9e48df448802b6f76f961284aa1c2fdb6c345b6
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-11 01:38:04 +0900

    新規 - MSWのかわりにFetchを自前でMockすることで、テストをできるようにする

---
+++ app/_mock/fetch/index.ts
@@ -0,0 +1,25 @@
+import { vi, beforeAll, afterAll } from 'vitest';
+
+interface IResponse<T> {
+  success: boolean;
+  status: number;
+  data?: T;
+}
+
+export const initMock = () => {
+  afterAll(() => {
+    vi.clearAllMocks();
+  });
+};
+
+export const createFetchMock = <T>({ success, status, data }: IResponse<T>) => {
+  const dataMock = () =>
+    new Promise((resolve) => {
+      resolve({
+        ok: success,
+        status: status,
+        json: async () => ({ data: { data } }),
+      });
+    });
+  global.fetch = vi.fn().mockImplementation(dataMock);
+};
