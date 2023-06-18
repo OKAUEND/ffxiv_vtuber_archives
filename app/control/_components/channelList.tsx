9be0f17d1a555e0dabf75340aab3fde33b113b95
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-18 21:55:30 +0900

    変更 - 配信者名やチャンネル名の項目を縦に並べて見やすくする

--- app/control/_components/channelList.tsx
+++ app/control/_components/channelList.tsx
@@ -5,17 +5,30 @@ export const ChannelList = () => {
   return (
     <section className={styles.container}>
       <h2>配信者一覧</h2>
-      <div className={styles.channels}>
-        <div className={styles.channel_sever}>◯</div>
-        <div className={styles.channel_icon}>
-          <Icon src="" alt="仮実装" />
-        </div>
-        <div className={styles.channel_info}>
-          <span>配信者名</span>
-          <span>チャンネル名</span>
-          <span>配信開始日</span>
-        </div>
-      </div>
+      <ul>
+        <li className={styles.channels}>
+          <div className={styles.channel_sever}>◯</div>
+          <div className={styles.channel_icon}>
+            <Icon src="" alt="仮実装" />
+          </div>
+          <div className={styles.channel_info}>
+            <span>配信者名</span>
+            <span>チャンネル名</span>
+            <span>配信開始日</span>
+          </div>
+        </li>
+        <li className={styles.channels}>
+          <div className={styles.channel_sever}>◯</div>
+          <div className={styles.channel_icon}>
+            <Icon src="" alt="仮実装" />
+          </div>
+          <div className={styles.channel_info}>
+            <span>配信者名</span>
+            <span>チャンネル名</span>
+            <span>配信開始日</span>
+          </div>
+        </li>
+      </ul>
     </section>
   );
 };
