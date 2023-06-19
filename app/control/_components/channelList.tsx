67f3b09bed34206608225e96d81e6c3fe515b436
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-19 19:59:57 +0900

    新規 - 配信者一覧をコンポーネント分割をする

---
+++ app/control/_components/channelList.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Icon } from '@/_components/Elements/Icon';
+import styles from '@/control/_styles/channelList.module.scss';
+import { useAdminControl } from '@/control/(hooks)/useAdminControl';
+
+export const ChannelList = () => {
+  const channels = useAdminControl();
+  return (
+    <ul>
+      {channels.map((channel, index) => (
+        <li className={styles.channels} key={index}>
+          <div className={styles.channel_sever}>◯</div>
+          <div className={styles.channel_icon}>
+            <Icon src={channel.channelIconID} alt="仮実装" />
+          </div>
+          <div className={styles.channel_info}>
+            <h3 className={styles.title}>{channel.name}</h3>
+            <span>{channel.channelName}</span>
+            <span>2013/8.13</span>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
