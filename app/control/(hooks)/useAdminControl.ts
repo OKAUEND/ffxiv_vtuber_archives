dbefba0019e3821f30b92576e887d68b3a47343f
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-19 19:58:29 +0900

    変更 - Typoを修正

--- app/control/(hooks)/useAdminControl.ts
+++ app/control/(hooks)/useAdminControl.ts
@@ -1,6 +1,6 @@
 import { createHikasenVtuberData } from '@/channels/__tests__/_mock';
 
-export const useAdminContorol = () => {
+export const useAdminControl = () => {
   //現在のタスクでは仮置きでMockデータを使う
   const data = createHikasenVtuberData('Mock');
 
