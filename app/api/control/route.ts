11a9423f887588719ad5d856a16b0ea28c59aba5
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-21 17:32:03 +0900

    変更 - 試しでPrismaのCRUDを試す

--- app/api/control/route.ts
+++ app/api/control/route.ts
@@ -1,9 +1,52 @@
 import { fetchExtend } from '@/_utile/fetch';
 import { HikasenVtuber } from '@/(types)/';
 
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+  log: ['query', 'error', 'info', 'warn'],
+});
+
 export async function GET() {
   const url = `${process.env.CHANNELLIST_URL}`;
 
   const channels = await fetchExtend<HikasenVtuber[]>({ url });
+  const test = await prisma.user.findMany();
   return new Response(JSON.stringify(channels));
+}
+
+export async function POST(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const name = searchParams.get('name');
+  const test = await prisma.user.create({
+    data: {
+      email: `Hiroshi@${name}`,
+      name: `Hiroshi${name}`,
+    },
+  });
+}
+
+export async function DELETE(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const name = searchParams.get('name');
+  const test = await prisma.user.deleteMany({
+    where: {
+      email: {
+        contains: `Hiroshi@${name}`,
+      },
+    },
+  });
+}
+
+export async function PUT(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const name = searchParams.get('name');
+  const test = await prisma.user.update({
+    where: {
+      email: `Hiroshi@HQ`,
+    },
+    data: {
+      name: 'HiroshiSuperHQ',
+    },
+  });
 }
