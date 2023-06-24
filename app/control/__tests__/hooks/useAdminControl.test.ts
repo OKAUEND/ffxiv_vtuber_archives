654fea9652e6425a06e031bf9304d9b13481f384
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-24 18:10:42 +0900

    新規 - Hooksで複雑なロジックを実装予定なので、単体テストを実装するためにファイルを作成

---
+++ app/control/__tests__/hooks/useAdminControl.test.ts
@@ -0,0 +1,5 @@
+import { describe, test, expect } from 'vitest';
+import { renderHook } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
+
+describe('useAdminControl Unit TEST', () => {});
