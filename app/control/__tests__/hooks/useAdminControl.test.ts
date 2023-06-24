06dfec4323e7cb37e2df1f6fa90307c63369a907
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-24 18:15:46 +0900

    変更 - テスト内容を作成する

--- app/control/__tests__/hooks/useAdminControl.test.ts
+++ app/control/__tests__/hooks/useAdminControl.test.ts
@@ -2,4 +2,9 @@ import { describe, test, expect } from 'vitest';
 import { renderHook } from '@testing-library/react';
 import { RecoilRoot } from 'recoil';
 
-describe('useAdminControl Unit TEST', () => {});
+describe('useAdminControl Unit TEST', () => {
+  test('値が取れているか', () => {});
+  test('DB側に値がない場合は、一致しているかのフラグが偽になっているか', () => {});
+  test('DB側に値があった場合は、全要素が一致していればフラグが真になっているか', () => {});
+  test('DB側に値があった場合は、一部要素が違っていればフラグが偽になっているか', () => {});
+});
