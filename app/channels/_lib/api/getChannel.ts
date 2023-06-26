88397ca34fa898e3418172117e3033fa03a7f43a
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-06-27 01:12:00 +0900

    変更 - ページネーションで、2ページ目以降での取得処理もprismaへ変更をする

--- app/channels/_lib/api/getChannel.ts
+++ app/channels/_lib/api/getChannel.ts
@@ -1,5 +1,5 @@
-import { fetchExtend } from '@/_utile/fetch';
 import { HikasenVtuber } from '@/(types)/';
+import prisma from '@/_utile/prisma';
 
 export const getChannel = async (offset: string): Promise<HikasenVtuber[]> => {
   const BASE_QUERY_COUNT = 20;
@@ -8,8 +8,11 @@ export const getChannel = async (offset: string): Promise<HikasenVtuber[]> => {
       ? ''
       : `?offset=${BASE_QUERY_COUNT * (Number(offset) - 1)}&limit=20`;
 
-  const URL = `${process.env.CHANNELLIST_URL}${query}`;
-  const data = await fetchExtend<HikasenVtuber[]>({ url: URL });
+  const res = await prisma.channel.findMany({
+    orderBy: {
+      isOfficial: 'desc',
+    },
+  });
 
-  return data;
+  return res;
 };
