aaf67b524dcd0f2a2e5b93bf182826fe3a621127
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-03 15:47:09 +0900

    変更 - ルート先のページで、URLParamからOffset数を生成し、DBより値を取得

--- app/channels/[pages]/page.tsx
+++ app/channels/[pages]/page.tsx
@@ -1,10 +1,14 @@
 import { Channel } from '@/channels/_components/router';
 
-export default async function Article() {
+export default async function Article({
+  params,
+}: {
+  params: { pages: string };
+}) {
   return (
     <>
       {/* @ts-expect-error Async Server Component */}
-      <Channel />
+      <Channel param={params.pages} />
     </>
   );
 }
--- app/channels/_lib/api/getChannel.ts
+++ app/channels/_lib/api/getChannel.ts
@@ -1,14 +1,19 @@
 import { HikasenVtuber } from '@/(types)/';
-import prisma, { getChannelOffset } from '@/_utile/prisma';
+import { getChannelOffset } from '@/_utile/prisma';
 
 export const getChannel = async (offset: string): Promise<HikasenVtuber[]> => {
+  //モバイルでのみやすさも考慮し、20件ほどに絞る。10件だけはPCやタブレットで見るには少なすぎる
   const BASE_QUERY_COUNT = 20;
-  const query =
-    Number(offset) === 1
-      ? ''
-      : `?offset=${BASE_QUERY_COUNT * (Number(offset) - 1)}&limit=20`;
+  //何も指定がないときのためのガード構文
+  const offsetNumber = offset ? Number(offset) : 1;
 
-  const res = getChannelOffset();
+  //ページ番号よりOffsetでずらす件数を作成し、+1することで、同じ要素の重複取得を防ぐ
+  const skip =
+    offsetNumber > 1
+      ? offsetNumber
+      : BASE_QUERY_COUNT * (Number(offset) - 1) + 1;
+
+  const res = getChannelOffset(skip);
 
   return res;
 };
