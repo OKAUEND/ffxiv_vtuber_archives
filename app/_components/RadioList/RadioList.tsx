8624e228de11b331626d034a9db15c9279a3327f
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-05 16:26:15 +0900

    変更 - ラジオボタン周りの表示と変更処理をラッパーするコンポーネントを作り、流用できるようにする

--- app/_components/RadioList/RadioList.tsx
+++ app/_components/RadioList/RadioList.tsx
@@ -2,6 +2,39 @@
 
 import styles from './radios.module.scss';
 
-export const RadioList = () => {
-  return <div></div>;
+interface Props {
+  categories: string[];
+  selected: string;
+  group: string;
+  changeHandler: (selected: string) => void;
+}
+
+export const RadioList = ({
+  categories,
+  selected,
+  group,
+  changeHandler,
+}: Props) => {
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    changeHandler(event.target.value);
+  };
+  return (
+    <ul className={styles.radio_container}>
+      {categories.map((category, index) => (
+        <li key={index} className={styles.radio_list}>
+          <label className={styles.radio_group}>
+            <input
+              className={styles.radio}
+              type="radio"
+              value={category}
+              name={group}
+              checked={selected === category}
+              onChange={onChange}
+            />
+            <span className={styles.radio_text}> {category}</span>
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
 };
