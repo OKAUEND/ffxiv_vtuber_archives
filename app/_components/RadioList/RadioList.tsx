d252a09c7f1d3dae801cb468b18126864e725475
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-05 23:51:57 +0900

    変更 - ラジオボタンに対するユニットテストを実装する

--- app/_components/RadioList/RadioList.tsx
+++ app/_components/RadioList/RadioList.tsx
@@ -35,7 +35,7 @@ export const RadioList = ({
               checked={selected === category}
               onChange={onChange}
             />
-            <span> {category}</span>
+            <span>{category}</span>
           </label>
         </li>
       ))}
