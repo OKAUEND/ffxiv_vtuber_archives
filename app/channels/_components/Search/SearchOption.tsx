da8e76822a9f2ce73ba1a693fedcdabefccf92f1
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-08 20:36:37 +0900

    変更 - 絞り込み検索に、配信開始日順の降順昇順ボタンを作成

--- app/channels/_components/Search/SearchOption.tsx
+++ app/channels/_components/Search/SearchOption.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { RadioList } from '@/_components/RadioList';
+
 import {
   useChannelSearchOption,
   sortOptions,
@@ -7,5 +9,19 @@ import {
 
 export const SearchOption = () => {
   const [] = useChannelSearchOption();
-  return <div></div>;
+  return (
+    <section>
+      <h2>絞り込み</h2>
+      <article>
+        <RadioList
+          categories={sortOptions}
+          selected="Desc"
+          group="Sort"
+          changeHandler={() => {
+            return;
+          }}
+        />
+      </article>
+    </section>
+  );
 };
