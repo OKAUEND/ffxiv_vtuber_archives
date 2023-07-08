824cf321770c3276f2874b35ff899efa0025f0a9
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-08 20:29:12 +0900

    変更 - 絞り込み検索によるユーザーの選択状況の状態を管理するHooksを作成

---
+++ app/channels/(hooks)/Search/useChannelSearchOption.ts
@@ -0,0 +1,8 @@
+export const sortOptions = [
+  { key: 'Desc', name: '新しい順' },
+  { key: 'Asc', name: '古い順' },
+];
+
+export const useChannelSearchOption = () => {
+  return [] as const;
+};
--- app/channels/_components/Search/SearchOption.tsx
+++ app/channels/_components/Search/SearchOption.tsx
@@ -1,5 +1,11 @@
 'use client';
 
+import {
+  useChannelSearchOption,
+  sortOptions,
+} from '@/channels/(hooks)/Search/useChannelSearchOption';
+
 export const SearchOption = () => {
+  const [] = useChannelSearchOption();
   return <div></div>;
 };
