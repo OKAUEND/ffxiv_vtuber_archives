a59c7fa1f5ae4426d09ee1908eba20389dc8bd99
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-11 20:35:40 +0900

    変更 - 検索条件で配信者を絞りたいので、検索を行うために必要なPrisma処理を用意する

--- app/_utile/prisma/index.ts
+++ app/_utile/prisma/index.ts
@@ -1,10 +1,15 @@
 import { HikasenVtuber } from '@/(types)';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 const prisma = new PrismaClient({
   log: ['query', 'error', 'info', 'warn'],
 });
 
+/**
+ * 配信者一覧を降順で無条件取得をする
+ * @param offset
+ * @returns
+ */
 export const getChannelOffset = async (
   offset = 0
 ): Promise<HikasenVtuber[]> => {
@@ -22,8 +27,52 @@ export const getChannelOffset = async (
   return res;
 };
 
+/**
+ * 配信者の件数を取得する
+ * @returns count number
+ */
 export const getChannelCount = async () => {
   const res = await prisma.channel.count();
+  return res;
+};
+
+/**
+ * 配信者を条件で検索し、一致した配信者を取得する
+ * @param offset 次回の取得を始めるスタート位置
+ * @param query 検索条件 - PrismaのWhereの型を利用しオブジェクトを作成し渡す
+ * @param orderBy 昇順降順の指定
+ * @returns
+ */
+export const getChannelWhereOffset = async (
+  offset = 0,
+  query: Prisma.ChannelWhereInput,
+  orderBy: Prisma.SortOrder
+): Promise<HikasenVtuber[]> => {
+  const res = await prisma.channel.findMany({
+    take: 20,
+    skip: offset,
+    where: {
+      isOfficial: false,
+      ...query,
+    },
+    orderBy: [{ beginTime: orderBy }],
+  });
+
+  return res;
+};
+
+/**
+ * 配信者を条件に一致する件数
+ * @param query 検索条件 - PrismaのWhereの型を利用しオブジェクトを作成し渡す
+ * @returns
+ */
+export const getChannelWhereCount = async (query: Prisma.ChannelWhereInput) => {
+  const res = await prisma.channel.count({
+    where: {
+      isOfficial: false,
+      ...query,
+    },
+  });
   return res;
 };
 
