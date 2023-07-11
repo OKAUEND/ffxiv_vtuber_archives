c550a134dd86b03f1e4581a1d95f387bb8059210
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-11 22:06:54 +0900

    変更 - 取得関数を呼び出す

---
+++ app/channels/(types)/index.ts
@@ -0,0 +1,5 @@
+export interface ChannelSearchParams {
+  page: string;
+  orderBy: string;
+  year: string;
+}
--- app/channels/_components/Result/router.tsx
+++ app/channels/_components/Result/router.tsx
@@ -1,3 +1,4 @@
+import { getChannelWhere } from '@/channels/_lib/api/getChannelWhere';
 import { ChannelPanel } from '@/channels/_components/ChannelPanel';
 import { ChannelSearchParams } from '@/channels/(types)';
 import { Pagination } from '@/_components/Pagination';
@@ -8,10 +9,11 @@ interface IProps {
 }
 
 export const ChannelResult = async ({ page, params }: IProps) => {
+  const [channels, count] = await getChannelWhere(params, page);
   return (
     <>
-      <ChannelPanel  />
-      <Pagination />
+      <ChannelPanel channels={channels} />
+      <Pagination basePath="/" currentPageNumber={1} totalCount={count} />
     </>
   );
 };
