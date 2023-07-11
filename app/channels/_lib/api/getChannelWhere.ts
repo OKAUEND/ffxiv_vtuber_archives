c4c96f6bc2a909916726d92a13131d75aff803b4
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-11 22:05:54 +0900

    変更 - 受け取ったクエリを元に、PrismaのWhere文を動的に作成する。 現在は1つしか無いので、IF文で判別するなどはしていない

--- app/channels/_lib/api/getChannelWhere.ts
+++ app/channels/_lib/api/getChannelWhere.ts
@@ -12,4 +12,30 @@ export const getChannelWhere = async (
 const createWhereQuery = (
   params: ChannelSearchParams
 ): Prisma.ChannelWhereInput => {
+  const keys = Object.entries(params);
+
+  const query = keys.reduce((acc, currentKey) => {
+    //ページ数をParameterで
+    if (currentKey[0] === 'orderBy') return acc;
+
+    //まずは配信時間のWhere文だけを作成する
+    //後にタグ検索とかを行いたいので、改修はしようね
+    const beginDayTime = new Date(currentKey[1]);
+    const endDayTime = new Date(currentKey[1]);
+    endDayTime.setMonth(12);
+    endDayTime.setSeconds(-1);
+
+    const where: Prisma.ChannelWhereInput = {
+      beginTime: {
+        //開始日時
+        gte: beginDayTime,
+        //終了日時
+        lt: endDayTime,
+      },
+    };
+
+    return where;
+  }, {});
+
+  return query;
 };
