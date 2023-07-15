f31289b6115208bb3c3b9df99f677a7662b23e03
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-15 18:48:44 +0900

    変更 - モバイル表示時に、画面外へ要素が飛び出すため、親サイズの最大幅に合わせるようにする

--- app/channels/_components/Search/SearchCategories.tsx
+++ app/channels/_components/Search/SearchCategories.tsx
@@ -7,34 +7,34 @@ import {
   useChannelSearchBeginYear,
 } from '@/channels/(hooks)/Search/useChannelSearchCategories';
 
+import styles from '@/channels/_style/search/search.module.scss';
+
 export const SearchCategories = () => {
   const [selectedOption, sortData, changeOption] = useChannelSearchOption();
   const [selectedYear, years, changeYear] = useChannelSearchBeginYear();
   return (
-    <section>
-      <form action="/api/form" method="GET">
-        <article>
-          <h2>絞り込み</h2>
-          <span>並び</span>
-          <RadioList
-            categories={sortData}
-            selected={selectedOption.key}
-            group="sort"
-            changeHandler={changeOption}
-          />
-        </article>
-        <article>
-          <h2>カテゴリー検索</h2>
-          <span>配信開始年</span>
-          <RadioList
-            categories={years}
-            selected={selectedYear.key}
-            group="year"
-            changeHandler={changeYear}
-          />
-        </article>
-        <button>検索する</button>
-      </form>
-    </section>
+    <form action="/api/form" method="GET" className={styles.container}>
+      <article>
+        <h2>絞り込み</h2>
+        <span>並び</span>
+        <RadioList
+          categories={sortData}
+          selected={selectedOption.key}
+          group="sort"
+          changeHandler={changeOption}
+        />
+      </article>
+      <article>
+        <h2>カテゴリー検索</h2>
+        <span>配信開始年</span>
+        <RadioList
+          categories={years}
+          selected={selectedYear.key}
+          group="year"
+          changeHandler={changeYear}
+        />
+      </article>
+      <button>検索する</button>
+    </form>
   );
 };
