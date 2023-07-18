661e3bfc70f956f874b719370c4d38f8826227b3
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-18 15:28:59 +0900

    変更 - AccordionのUnitTESTがコーディングが完成

--- app/_components/Accordion/__tests__/Accordion.test.tsx
+++ app/_components/Accordion/__tests__/Accordion.test.tsx
@@ -1,5 +1,34 @@
 import { describe, test, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { RecoilRoot } from 'recoil';
 
-describe('Accordion Component TEST', () => {});
+import { Accordion } from '../Accordion';
+
+describe('Accordion Component TEST', () => {
+  const TestComponent = () => <div>Component TEST</div>;
+  test('ボタンにPropsのTitleの要素が表示されているか', async () => {
+    render(
+      <RecoilRoot>
+        <Accordion title="Accordion Button">
+          <TestComponent />
+        </Accordion>
+      </RecoilRoot>
+    );
+
+    const ButtonTEXT = await screen.getByText('Accordion Button');
+    expect(ButtonTEXT.textContent).toEqual('Accordion Button');
+  });
+  test('子のComponentは表示できているか', async () => {
+    render(
+      <RecoilRoot>
+        <Accordion title="Accordion Button">
+          <TestComponent />
+        </Accordion>
+      </RecoilRoot>
+    );
+
+    const ChildTEXT = await screen.getByText('Component TEST');
+    expect(ChildTEXT.textContent).toEqual('Component TEST');
+  });
+});
