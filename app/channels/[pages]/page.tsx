c857b7264e45a35482cfb9089e685274dd34fd7c
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-23 16:21:37 +0900

    変更 - 2ページ以降では、検索ページへの案内が表示されていなかったので、表示する

--- app/channels/[pages]/page.tsx
+++ app/channels/[pages]/page.tsx
@@ -2,6 +2,7 @@ import styles from '@/_styles/rootPage.module.scss';
 
 import { getChannel } from '@/channels/_lib/api/getChannel';
 import { ChannelIndex } from '@/channels/_components/ChannelIndex';
+import Link from 'next/link';
 
 export default async function Article({
   params,
@@ -11,6 +12,11 @@ export default async function Article({
   const [channels, count] = await getChannel(params.pages);
   return (
     <div className={styles.content}>
+      <div className={styles.search_container}>
+        <Link className={styles.link} href={'/channels/search'}>
+          配信者を探す→
+        </Link>
+      </div>
       <ChannelIndex
         totalCount={count}
         channels={channels}
