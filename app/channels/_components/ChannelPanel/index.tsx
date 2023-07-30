0a6de99feedd6ccd8132ef169607073c8c0abe10
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-07-30 22:43:22 +0900

    変更 - 一覧から直接Youtubeへ飛びたいので、Link先を変更する

--- app/channels/_components/ChannelPanel/index.tsx
+++ app/channels/_components/ChannelPanel/index.tsx
@@ -15,29 +15,32 @@ export const ChannelPanel = ({ channels }: Props) => {
     <ul className={styles.container}>
       {channels.map((channel) => (
         <li key={channel.channelID} className={styles.channel_list}>
-          <Link href={`/archives/${channel.channelID}`}>
-            <div className={styles.channel_content}>
+          <div className={styles.channel_content}>
+            <Link
+              href={`https://www.youtube.com/channel/${channel.channelID}`}
+              target="_blank"
+            >
               <div className={styles.channel_icon}>
                 <Icon
                   src={`${youtubeIconURL}${channel.channelIconURL}`}
                   alt={`${channel.name}のチャンネルアイコン`}
                 />
               </div>
-              <div className={styles.channel_info}>
-                <div className={styles.info_title}>
-                  <span className={styles.info_text}>{channel.name}</span>
-                  <span className={styles.info_channelName}>
-                    {channel.channelName}
-                  </span>
-                </div>
-
-                <span className={styles.channel_since}>
-                  {DayTime(channel.beginTime)}
+            </Link>
+            <div className={styles.channel_info}>
+              <div className={styles.info_title}>
+                <span className={styles.info_text}>{channel.name}</span>
+                <span className={styles.info_channelName}>
+                  {channel.channelName}
                 </span>
               </div>
-              <div className={styles.channel_tag}></div>
+
+              <span className={styles.channel_since}>
+                {DayTime(channel.beginTime)}
+              </span>
             </div>
-          </Link>
+            <div className={styles.channel_tag}></div>
+          </div>
         </li>
       ))}
     </ul>
