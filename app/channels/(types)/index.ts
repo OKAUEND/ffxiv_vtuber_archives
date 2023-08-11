e09e5ae56dd63797f69abca88a60bd6e3de18414
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-08-12 02:43:08 +0900

    変更 - 型を変更

--- app/channels/(types)/index.ts
+++ app/channels/(types)/index.ts
@@ -1,4 +1,19 @@
+import { Prisma } from '@prisma/client';
+
 export interface ChannelSearchParams {
   orderBy: string;
   year: string;
+  content: string[];
+  play: string[];
+  timezone: [];
+}
+
+export interface PrismaQuery {
+  query: {
+    content: Prisma.ChannelWhereInput;
+    play: Prisma.ChannelWhereInput;
+    timeZone: Prisma.ChannelWhereInput;
+  };
+  year: Prisma.ChannelWhereInput;
+  orderBy: Prisma.SortOrder;
 }
