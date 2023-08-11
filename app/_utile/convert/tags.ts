b103e8219fe4833fce34a3918a8ba8fca5b96ef4
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-08-12 02:44:55 +0900

    変更 - タグ一覧をカテゴリー毎に分類をする関数を作成

--- app/_utile/convert/tags.ts
+++ app/_utile/convert/tags.ts
@@ -59,3 +59,54 @@ export const convertTaggingToTags = (channels): HikasenVtuber<Tags>[] => {
     };
   });
 };
+
+export const convertTagsToCategoryTags = (tags: PrismaTags[] | Tag[]): Tags => {
+  //DBのタグ情報は、1つのテーブルに入っているので、種類毎に使いやすいように分割する
+  const contents: Tag[] = [];
+  const party: Tag[] = [];
+  const timezone: Tag[] = [];
+
+  tags.forEach((tag) => {
+    //ソートのために必要なID、表示に必要な名前、コードのみにオブジェクトを限定する
+    const convert = {
+      id: tag.id,
+      name: tag.name,
+      code: tag.code,
+      type: tag.type,
+    };
+
+    //見やすいようにSwitch文で切り分ける
+    switch (tag.type) {
+      case 'content':
+        contents.push(convert);
+        break;
+      case 'play':
+        party.push(convert);
+        break;
+      case 'timezone':
+        timezone.push(convert);
+        break;
+      default:
+        break;
+    }
+  });
+
+  //サーバー登録順は必ずしもID順に並んでいないので、ID順に並べ直させる
+  contents.sort((a, b) => {
+    return a.id - b.id;
+  });
+  party.sort((a, b) => {
+    return a.id - b.id;
+  });
+  timezone.sort((a, b) => {
+    return a.id - b.id;
+  });
+
+  const convertTags = {
+    content: contents,
+    party: party,
+    timezone: timezone,
+  };
+
+  return convertTags;
+};
