51dc447e5787a926c642a604dbf2c4bad9a97720
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-08-21 22:05:57 +0900

    変更 - Where文の有無で分けていた関数を一つに統合する

--- app/_utile/prisma/index.ts
+++ app/_utile/prisma/index.ts
@@ -9,53 +9,13 @@ export const prisma = new PrismaClient({
 });
 
 /**
- * 配信者一覧を降順で無条件取得をする
- * @param offset
- * @returns
- */
-export const getChannelOffset = async (
-  offset = 0
-): Promise<HikasenVtuber[]> => {
-  const channels = await prisma.channel.findMany({
-    take: 20,
-    skip: offset,
-    orderBy: [
-      {
-        isOfficial: 'desc',
-      },
-      { beginTime: 'desc' },
-    ],
-    include: {
-      tags: {
-        include: {
-          tags: true,
-        },
-      },
-    },
-  });
-
-  const result = convertTaggingToTags(channels);
-
-  return result;
-};
-
-/**
- * 配信者の件数を取得する
- * @returns count number
- */
-export const getChannelCount = async () => {
-  const res = await prisma.channel.count();
-  return res;
-};
-
-/**
  * 配信者を条件で検索し、一致した配信者を取得する
  * @param offset 次回の取得を始めるスタート位置
  * @param query 検索条件 - PrismaのWhereの型を利用しオブジェクトを作成し渡す
  * @param orderBy 昇順降順の指定
  * @returns
  */
-export const getChannelWhereOffset = async (
+export const getChannelResult = async (
   offset = 0,
   query: PrismaQuery
 ): Promise<HikasenVtuber<Tags>[]> => {
@@ -92,10 +52,8 @@ export const getChannelWhereOffset = async (
  * @param query 検索条件 - PrismaのWhereの型を利用しオブジェクトを作成し渡す
  * @returns
  */
-export const getChannelWhereCount = async (offset = 0, query: PrismaQuery) => {
+export const getChannelResultCount = async (query: PrismaQuery) => {
   return await prisma.channel.count({
-    take: 20,
-    skip: offset,
     where: {
       AND: [query.query.content, query.query.play, query.query.timeZone],
       isOfficial: false,
