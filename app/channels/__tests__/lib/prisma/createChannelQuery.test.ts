15ad9ef7b75c726c4150796bef7ed5b3391e3dd9
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-08-25 19:20:32 +0900

    変更 - クエリパラメータがなにもなかった時には、クエリが生成されない

--- app/channels/__tests__/lib/prisma/createChannelQuery.test.ts
+++ app/channels/__tests__/lib/prisma/createChannelQuery.test.ts
@@ -38,7 +38,21 @@ const createQueryFactory: createQueryFactory = ({
 };
 
 describe('createChannelQuery Unit TEST', () => {
-  test('クエリパラメータが何も指定されていない時は、プロパティに空のオブジェクトが入ったQueryオブジェクトになる', () => {});
+  test('クエリパラメータが何も指定されていない時は、プロパティに空のオブジェクトが入ったQueryオブジェクトになる', () => {
+    const query = createWhereQuery();
+
+    const date: PrismaQuery = {
+      query: {
+        content: {},
+        play: {},
+        timeZone: {},
+      },
+      year: {},
+      orderBy: 'desc',
+    };
+
+    expect(query).toEqual(date);
+  });
   test('ソート順を指定するクエリパラメータがある時、指定通りにソートのプロパティが設定されているか', () => {});
   test('指定年のクエリパラメータがある時、指定年の開始日が1月1日で終了日が12月31日になっているか', () => {});
   test('プレイスタイル:contentのクエリパラメータがある時、PrismaでINNER JOINをし、OR文になっているか', () => {});
