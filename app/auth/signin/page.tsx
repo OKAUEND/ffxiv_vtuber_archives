1236a4527e6f7a1efc85f11765314121f35164fe
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-08-25 22:07:05 +0900

    変更 - デザインのために2カラム構成にするため、2つに分割する用意をする

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -5,13 +5,16 @@ export default function AuthSignIn() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <div className={styles.message}>
-          <span>お持ちのアカウントを使いログインをします</span>
-          <span>※現在ログイン機能は停止中です</span>
-        </div>
-        <SignIn provider="github">
-          <span className={styles.signin_text}>Googleでログイン</span>
-        </SignIn>
+        <main>
+          <div className={styles.message}>
+            <span>お持ちのアカウントを使いログインをします</span>
+            <span>※現在ログイン機能は停止中です</span>
+          </div>
+          <SignIn provider="github">
+            <span className={styles.signin_text}>Googleでログイン</span>
+          </SignIn>
+        </main>
+        <aside>暁月</aside>
       </div>
     </div>
   );
