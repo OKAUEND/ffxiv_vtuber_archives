c2583dbf96ff20031036b75103405e4aeacb5fe4
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-09-04 16:15:56 +0900

    変更 - getTagsをHooks経由で取得していたのを、Prismaで取得する事をみなしてサーバ側で取得し渡す

--- app/channels/_components/Result/index.tsx
+++ app/channels/_components/Result/index.tsx
@@ -1,4 +1,4 @@
-import { getChannel } from '@/_prisma';
+import { getChannel, getTags } from '@/_prisma';
 import { ChannelSearchParams } from '@/channels/(types)';
 import { Accordion } from '@/_components/Accordion';
 
@@ -7,6 +7,7 @@ import { SearchCategories } from '@/channels/_components/Search/SearchCategories
 import { ChannelIndex } from '@/channels/_components/ChannelIndex';
 
 import styles from '@/_styles/rootPage.module.scss';
+import { convertTagsToCategoryTags } from '@/_utile/convert';
 
 interface IProps {
   page: string;
@@ -15,11 +16,12 @@ interface IProps {
 
 export const ChannelResult = async ({ page, params }: IProps) => {
   const [channels, count] = await getChannel(page, params);
+  const tags = convertTagsToCategoryTags(getTags());
 
   return (
     <section className={styles.content}>
       <Accordion title="さらにVtuberを探す">
-        <SearchCategories params={params} />
+        <SearchCategories params={params} tags={tags} />
       </Accordion>
       <ChannelIndex
         page={page}
--- app/channels/search/page.tsx
+++ app/channels/search/page.tsx
@@ -1,5 +1,8 @@
 import { SearchCategories } from '@/channels/_components/Search/SearchCategories';
+import { getTags } from '@/_prisma';
+import { convertTagsToCategoryTags } from '@/_utile/convert';
 
 export default async function ChannelSearchIndex() {
-  return <SearchCategories />;
+  const tags = convertTagsToCategoryTags(getTags());
+  return <SearchCategories tags={tags} />;
 }
